a787fc0bc31af8639f4a46876145bcd6cac03bae
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2010-07-31 13:00:31 +0000

    year, copyright

--- wp-content/themes/cartoonbank3/footer.php
+++ wp-content/themes/cartoonbank3/footer.php
@@ -4,10 +4,10 @@
 <div style="clear:both;"></div>
 
 <div id="footer">
-Copyright &copy; 2007-2010 <a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a>
+Copyright &copy; 2010 <a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a>
 </div>
 
 <?php do_action('wp_footer'); ?>
-
+<br><br><br><br><br>
 </body>
 </html>
