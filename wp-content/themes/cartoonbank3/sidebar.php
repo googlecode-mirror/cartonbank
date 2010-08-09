e43deca4afdde7d07ba74dac84cb66e2210aba27
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2010-08-09 18:52:21 +0000

    fixing number of Categories calculation

--- wp-content/themes/cartoonbank3/sidebar.php
+++ wp-content/themes/cartoonbank3/sidebar.php
@@ -23,7 +23,7 @@
 global $wpdb;
 // Categories
     $categories = $wpdb->get_results("SELECT * FROM `".$wpdb->prefix."product_categories` WHERE `active`='1' AND `category_parent` = '0' ORDER BY `order` ASC",ARRAY_A);
-	$category_count = $wpdb->get_results("SELECT `wp_item_category_associations`.`category_id`, COUNT(*) as count FROM `wp_product_list`,`wp_item_category_associations` WHERE `wp_product_list`.`active`='1' AND visible='1' AND `wp_product_list`.`id` = `wp_item_category_associations`.`product_id` GROUP BY `wp_item_category_associations`.`category_id`;",ARRAY_A);
+	$category_count = $wpdb->get_results("SELECT `wp_item_category_associations`.`category_id`, COUNT(`wp_product_list`.`id`) as count FROM `wp_product_list`,`wp_item_category_associations` WHERE `wp_product_list`.`active`='1' AND `wp_product_list`.visible='1' AND `wp_product_list`.`brand` in (SELECT DISTINCT id FROM `wp_product_brands`) AND `wp_product_list`.`id` = `wp_item_category_associations`.`product_id` GROUP BY `wp_item_category_associations`.`category_id`;",ARRAY_A);
 	$total_cartoons = 0;
 	if($category_count != null)
 	{
