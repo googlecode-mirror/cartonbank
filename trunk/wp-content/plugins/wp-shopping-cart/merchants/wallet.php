c52c0d1a79fd1f5ba6819d1963ab8a2d886783fc
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2010-08-19 13:34:01 +0000

    wallet write to the table

--- trunk/wp-content/plugins/wp-shopping-cart/merchants/wallet.php
+++ trunk/wp-content/plugins/wp-shopping-cart/merchants/wallet.php
@@ -17,7 +17,7 @@ function gateway_wallet($seperator, $sessionid)
     $userdata->wallet = $userdata->wallet - $totalsum;
     $wpdb->query("UPDATE `".$wpdb->prefix."users` SET `wallet` = ".$userdata->wallet." WHERE `id` ='".$user_ID."' LIMIT 1 ;");
     $transact_url = get_option('transact_url');
-    $sql = "UPDATE `WP_PURCHASE_LOGS` SET `processed`= '2' WHERE `sessionid`=".$sessionid;
+    $sql = "UPDATE `wp_purchase_logs` SET `processed`= '2' WHERE `sessionid`=".$sessionid;
     $wpdb->query($sql);
     $_SESSION['wallet'] = 'success';
   }
