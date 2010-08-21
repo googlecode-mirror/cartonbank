61fb9f9c7dd62351dd03bc494c2a580a0782117c
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2010-08-21 15:48:35 +0000

    filter by brand in the edit form

--- trunk/wp-content/plugins/wp-shopping-cart/display-items.php
+++ trunk/wp-content/plugins/wp-shopping-cart/display-items.php
@@ -515,7 +515,7 @@ if(isset($_GET['catid']) && is_numeric($_GET['catid']))
 
    $category_count = $wpdb->get_results("SELECT COUNT(*) as count FROM `wp_product_list`,`wp_item_category_associations` WHERE `wp_product_list`.`active`='1' ".$visiblesql." AND `wp_product_list`.`id` = `wp_item_category_associations`.`product_id` AND `wp_item_category_associations`.`category_id`='".$_GET['catid']."'",ARRAY_A);
    }
-   else if (isset($_GET['catid']) && is_numeric($_GET['brand']))
+   else if (isset($_GET['brand']) && is_numeric($_GET['brand']))
    {
 		// if we are getting items from only one brand
 		$sql = "SELECT `wp_product_list`.* FROM `wp_product_list`  WHERE `wp_product_list`.`active`='1' ".$visiblesql." AND `wp_product_list`.`brand`='".$_GET['brand']."' order by wp_product_list.id DESC LIMIT ".$offset.",".$items_on_page;
