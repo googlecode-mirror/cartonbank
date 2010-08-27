3395f7162d0e02d1d8de277fbc011bf85c642959
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2010-08-27 14:56:47 +0000

    removed debug email

--- wp-content/plugins/wp-shopping-cart/wp-shopping-cart-al.php
+++ wp-content/plugins/wp-shopping-cart/wp-shopping-cart-al.php
@@ -902,7 +902,7 @@ function nzshpcrt_submit_ajax()
             }
         }
       }
-    mail("aleshin@dataart.com","test",print_r($_REQUEST,true));
+    //mail("aleshin@dataart.com","test",print_r($_REQUEST,true));
     if((($item_data[0]['quantity_limited'] == 1) && ($item_data[0]['quantity'] != 0) && ($item_data[0]['quantity'] > $item_quantity)) || ($item_data[0]['quantity_limited'] == 0)) 
       {
       $cartcount = count($_SESSION['nzshpcrt_cart']);
