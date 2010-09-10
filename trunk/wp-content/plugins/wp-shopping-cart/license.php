e40679eeef8fdb2ec01959256a633680f95f4ad6
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2010-09-10 10:29:35 +0000

    license generate

---
+++ trunk/wp-content/plugins/wp-shopping-cart/license.php
@@ -0,0 +1,94 @@
+<?
+require('/home/www/cb/wp-blog-header.php');
+
+if (isset($_GET['l']) && is_numeric($_GET['l']))
+ $license_num = ($_GET['l']);
+
+if (isset($_GET['item']) && is_numeric($_GET['item']))
+ get_license($_GET['item'],$license_num);
+
+function get_license($sequence_of_image,$license_num)
+{
+	/*
+	#agreement_number#
+	#agreement_date#
+	#customer_name#
+	#image_number#
+	#author_name#
+	#media_name#
+	#price#
+*/
+
+// load unique license data
+$current_user = wp_get_current_user();    
+
+$agreement_number = uniqid();
+$agreement_date = date("m.d.y");
+$customer_name = $current_user->last_name. " " . $current_user->first_name;
+$media_name = '[не указано]';
+
+if(isset($_SESSION['nzshpcrt_cart']))
+{
+	$price = $_SESSION['nzshpcrt_cart'][$sequence_of_image] -> price;
+	$image_name = $_SESSION['nzshpcrt_cart'][$sequence_of_image] -> name;
+	$image_number = $_SESSION['nzshpcrt_cart'][$sequence_of_image] -> product_id;
+	$author_name = $_SESSION['nzshpcrt_cart'][$sequence_of_image] -> author;
+}
+
+//load Livense template
+$filename = '';
+switch($license_num)
+        {
+        case 1:
+        $filename = getcwd()."/"."Livense_limited_template.htm";
+        break;
+        
+        case 2:
+        $filename = getcwd()."/"."Livense_standard_template.htm";
+        break;
+
+        case 3:
+        $filename = getcwd()."/"."Livense_extended_template.htm";
+        break;
+
+        default:
+        $filename = getcwd()."/"."Livense_limited_template.htm";
+        break;
+}
+
+$content=loadFile($filename); 
+
+// replace placeholders
+$content = str_replace ('#agreement_number#',$agreement_number,$content);
+$content = str_replace ('#agreement_date#',$agreement_date,$content);
+$content = str_replace ('#customer_name#',$customer_name,$content);
+$content = str_replace ('#image_number#',$image_number,$content);
+$content = str_replace ('#image_name#',$image_name,$content);
+$content = str_replace ('#author_name#',$author_name,$content);
+$content = str_replace ('#media_name#',$media_name,$content);
+$content = str_replace ('#price#',$price,$content);
+
+// output content
+echo $content;
+}
+
+function loadFile($sFilename, $sCharset = 'UTF-8')
+{
+    if (floatval(phpversion()) >= 4.3) {
+        $sData = file_get_contents($sFilename);
+    } else {
+        if (!file_exists($sFilename)) return -3;
+        $rHandle = fopen($sFilename, 'r');
+        if (!$rHandle) return -2;
+
+        $sData = '';
+        while(!feof($rHandle))
+            $sData .= fread($rHandle, filesize($sFilename));
+        fclose($rHandle);
+    }
+    if ($sEncoding = mb_detect_encoding($sData, 'auto', true) != $sCharset)
+        $sData = mb_convert_encoding($sData, $sCharset, $sEncoding);
+    return $sData;
+}
+
+?>
