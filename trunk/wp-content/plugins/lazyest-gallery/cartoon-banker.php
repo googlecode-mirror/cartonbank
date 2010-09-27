f2be0ff7b0b1822a0a49da5cc8f5f87b1828dc2d
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2010-09-27 16:01:41 +0000

    input2 page rename

--- trunk/wp-content/plugins/lazyest-gallery/cartoon-banker.php
+++ trunk/wp-content/plugins/lazyest-gallery/cartoon-banker.php
@@ -10,7 +10,7 @@ Author URI: http://cartoonist.name
 function add_menu(){
 add_menu_page('Cartoon Banker','Банкир','read','lazyest-gallery/al-admin-panel.php');
 //add_submenu_page('lazyest-gallery/al-admin-panel.php','Input1', 'Отправка в базу', 'read', 'lazyest-gallery/mass-upload.php');
-add_submenu_page('lazyest-gallery/al-admin-panel.php','Input2', 'Редактор базы', 'read', 'wp-shopping-cart/display-items.php');
+add_submenu_page('lazyest-gallery/al-admin-panel.php','Редактор базы изображений', 'Редактор базы', 'read', 'wp-shopping-cart/display-items.php');
 }
 
 add_action('admin_menu','add_menu');
