e2806991522738f0e764cc2b10dd9639a0d93b7d
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2010-11-24 10:03:18 +0000

    склонение чисел в левом ухе

--- wp-content/themes/cartoonbank3/header.php
+++ wp-content/themes/cartoonbank3/header.php
@@ -39,12 +39,30 @@
 <?
 $cartoon_number = $wpdb->get_results("SELECT count( l.id ) AS cartoon_number FROM `wp_product_list` AS l, `wp_item_category_associations` AS a WHERE l.id = a.product_id AND l.active =1 AND l.visible =1 AND a.category_id in (select id from wp_product_categories WHERE active = 1 and id <> '666') AND l.brand in (SELECT DISTINCT id FROM `wp_product_brands` where `wp_product_brands`.active = 1)");
 $cartoon_number = $cartoon_number[0]->cartoon_number;
+$cartoon_number = $cartoon_number + 1; // strange difference??? strlen($cartoon_number-1)
+
+$switcher = substr($cartoon_number,strlen($cartoon_number)-1,1);
+
+switch ($switcher)
+{
+case"1":
+	$license_text = "лицензионное<br>изображение";
+	break;
+case"2":
+case"3":
+case"4":
+	$license_text = "лицензионных<br>изображения";
+	break;
+default:
+	$license_text = "лицензионных<br>изображений";
+	break;
+}
 ?>
 
 
 <div id="header">
 <table style="width:100%;margin:0;padding:0;"><tr>
-<td style="font-size:.8em;color:white;vertical-align:middle;width:185px;height:45px;padding:4px;background-color:#668bb7"><span style="color:#13223f;font-size:2em;"><b><? echo ($cartoon_number + 1);?></b></span><br>лицензионных<br>изображений</td>
+<td style="font-size:.8em;color:white;vertical-align:middle;width:185px;height:45px;padding:4px;background-color:#668bb7"><span style="color:#13223f;font-size:2em;"><b><? echo ($cartoon_number);?></b></span><br><?echo ($license_text);?></td>
 <td>
 	<a href="<?php echo get_option('home'); ?>/"><img src="<?php echo get_option('home'); ?>/wp-admin/images/cb-logo.gif" border="0"></a><br />
 	<?php bloginfo('description'); ?>
