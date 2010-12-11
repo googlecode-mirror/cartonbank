8cdb4902bbf57accc1783c2f6664d331d9736526
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2010-12-11 21:25:39 +0000

    purgatory

---
+++ trunk/wp-content/plugins/purgatory/purgatory.php
@@ -0,0 +1,211 @@
+<h3>Чистилище</h3>
+
+<script type="text/javascript" src="http://localhost/wp-content/plugins/purgatory/jquery.js"></script>
+<script type="text/javascript">
+	$(function() {
+	$(".vote").click(function() 
+	{
+	var id = $(this).attr("id");
+	var name = $(this).attr("name");
+	var dataString = 'id='+ id ;
+	var parent = $(this);
+
+
+	if(name=='up')
+	{
+	$(this).fadeIn(200).html('<img src="dot.gif" align="absmiddle">');
+	$.ajax({
+	   type: "POST",
+	   url: "http://localhost/wp-content/plugins/purgatory/up_vote.php",
+	   data: dataString,
+	   cache: false,
+
+	   success: function(html)
+	   {
+		parent.html(html);
+	  
+	  }  });
+	  
+	}
+	else
+	{
+	$(this).fadeIn(200).html('<img src="dot.gif" align="absmiddle">');
+	$.ajax({
+	   type: "POST",
+	   url: "http://localhost/wp-content/plugins/purgatory/down_vote.php",
+	   data: dataString,
+	   cache: false,
+
+	   success: function(html)
+	   {
+		   parent.html(html);
+	  }
+	   
+	 });
+
+	}
+	  
+
+	return false;
+		});
+
+	});
+</script>
+<script>
+
+function sendup(id)
+   {
+	var myelemname = "up"+id;
+	var mydiv = document.getElementById(myelemname);
+	ajax.post("http://localhost/wp-content/plugins/purgatory/up_vote.php", function(html){ mydiv.textContent=html;},"&id="+id);
+   }
+
+function senddown(id)
+   {
+	var myelemname = "down"+id;
+	var mydiv = document.getElementById(myelemname);
+	ajax.post("http://localhost/wp-content/plugins/purgatory/down_vote.php", function(html){ mydiv.textContent=html;},"&id="+id);
+   }
+
+</script>
+
+<style type="text/css">
+	#main
+	{
+	height:140px; border:1px dashed #29ABE2;margin-bottom:7px;
+	width:500px;
+	}
+	a
+	{
+	color:#DF3D82;
+	text-decoration:none;
+
+	}
+	a:hover
+	{
+	color:#DF3D82;
+	text-decoration:underline;
+
+	}
+	.up
+	{
+	padding-top: 8px;
+	height:30px; font-size:24px; text-align:center; background-color:#009900; margin-bottom:2px;
+	-moz-border-radius: 6px;-webkit-border-radius: 6px;
+	}
+	.up a
+	{
+	color:#FFFFFF;
+	text-decoration:none;
+
+	}
+	.up a:hover
+	{
+	color:#FFFFFF;
+	text-decoration:none;
+
+	}
+	.down
+	{
+	padding-top: 8px;
+	height:30px; font-size:24px; text-align:center; background-color:#cc0000; margin-top:2px;
+	-moz-border-radius: 6px;-webkit-border-radius: 6px;
+	}
+
+	.down a
+	{
+	color:#FFFFFF;
+	text-decoration:none;
+	}
+	.down a:hover
+	{
+	color:#FFFFFF;
+	text-decoration:none;
+	}
+	.box1
+	{
+	font-family:'Georgia', Times New Roman, Times, serif;
+	float:left; height:140px; width:50px;
+	}
+
+	.box2
+	{
+	float:left; width:150px; text-align:left;
+	margin-left:10px;height:140px;margin-top:0px;
+	font-weight:bold;
+
+	.box3
+	{
+	float:left; width:200px; text-align:left;
+	margin-left:10px;height:140px;margin-top:0px;
+	font-weight:bold;
+
+
+	.descr
+	{
+	clear:all; float:left; width:120px; text-align:left;
+	height:100px;
+
+
+	.img
+	{
+	float:left; width:200px; text-align:left;
+	margin-left:10px;height:140px;vertical-align: top;
+	}
+
+	img
+	{
+	border:none;
+	}
+	</style>
+
+<div>
+
+	<?php
+		include("config.php");
+
+		$sql=mysql_query("SELECT V.image_id, V.up, V.down, P.name, P.image, B.name AS Artist, P.approved FROM al_editors_votes AS V, wp_product_list AS P, wp_product_brands AS B 
+							WHERE V.image_id=P.id 
+							AND P.brand = B.id
+							AND P.approved is NULL
+							Limit 40");
+
+		while($row=mysql_fetch_array($sql))
+		{
+		$msg=$row['image_id'];
+		$mes_id=$row['image_id'];
+		$up=$row['up'];
+		$down=$row['down'];
+		$img=$row['image'];
+		$imgpath = "http://localhost/wp-content/plugins/wp-shopping-cart/images/".$img; 
+		$previewpath = "http://localhost/wp-content/plugins/wp-shopping-cart/product_images/".$img;
+		$imgname=$row['name'];
+		$artist=$row['Artist'];
+		?>
+
+	<div id="main">
+		<div class="box1">
+			<div class='up'><a href="" id="up<?php echo $mes_id;?>"  onclick="sendup(<?php echo $mes_id; ?>);return false;" class="vote" id="<?php echo $mes_id; ?>" name="up"><?php echo $up; ?></a></div>
+			<div class='down'><a href="" id="down<?php echo $mes_id;?>" onclick="senddown(<?php echo $mes_id; ?>);return false;" class="vote" id="<?php echo $mes_id; ?>" name="down"><?php echo $down; ?></a></div>
+		</div>
+
+		<div class='box2' >
+			<?php //echo $row['image_id']; ?>
+			<div class="img">
+				<a href="<? echo ($previewpath); ?>" target=_blank><img src="<? echo ($imgpath); ?>"></a>
+			</div>
+		</div>
+
+		<div class="box3">
+			Название: <? echo ($imgname);?><br>
+			Автор: <? echo ($artist);?><br>
+			Номер: <? echo ($mes_id);?>
+		</div>
+
+
+	</div>
+
+
+	<?php }?>
+
+</div>
---
+++ trunk/wp-content/plugins/purgatory/up_vote.php
@@ -0,0 +1,48 @@
+<?php
+include("config.php");
+
+$ip=$_SERVER['REMOTE_ADDR']; 
+
+if($_POST['id'])
+{
+$id=$_POST['id'];
+$id = mysql_escape_String($id);
+
+$ip_sql=mysql_query("select ip_add from al_editors_voting_ip where mes_id_fk='$id' and ip_add='$ip'");
+$count=mysql_num_rows($ip_sql);
+
+	if($count==0)
+	{
+		$sql = "update al_editors_votes set up=up+1  where image_id='$id'";
+		mysql_query( $sql);
+		
+
+		$sql_in = "insert into al_editors_votes (mes_id_fk,ip_add) values ('$id','$ip')";
+		mysql_query( $sql_in);
+	}
+
+$result=mysql_query("select up from al_editors_votes where image_id='$id'");
+
+
+$row=mysql_fetch_array($result);
+$up_value=$row['up'];
+
+		//fw("\n\r up_value=".$up_value);
+		if ($up_value >= $limit)
+		{
+			$sql = "update wp_product_list set approved=1 where id='$id'";
+			//fw("\n\r sql=".$sql);
+			mysql_query( $sql);
+		}
+
+echo $up_value;
+}
+
+function fw($text)
+{
+	$fp = fopen('_kloplog.txt', 'a');
+	fwrite($fp, $text);
+	fclose($fp);
+}
+
+?>
