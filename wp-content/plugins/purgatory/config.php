82e1b735db59aca6ec49e51bdf684f1a76c93df7
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2010-12-11 21:25:39 +0000

    purgatory

---
+++ wp-content/plugins/purgatory/config.php
@@ -0,0 +1,13 @@
+<?php
+$mysql_hostname = "localhost";
+$mysql_user = "z58365_cbru3";
+$mysql_password = "greenbat";
+$mysql_database = "cartoonbankru";
+
+$prefix = "";
+$limit = 3;
+
+$bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
+mysql_select_db($mysql_database, $bd) or die("Could not select database");
+
+?>
---
+++ wp-content/plugins/purgatory/down_vote.php
@@ -0,0 +1,50 @@
+<?php
+include("config.php");
+
+$ip=$_SERVER['REMOTE_ADDR']; 
+
+
+if($_POST['id'])
+{
+	$id=$_POST['id'];
+	$id = mysql_escape_String($id);
+
+	$ip_sql=mysql_query("select ip_add from al_editors_voting_ip where mes_id_fk='$id' and ip_add='$ip'");
+	
+	$count=mysql_num_rows($ip_sql);
+
+	if($count==0)
+	{
+		$sql = "update al_editors_votes set down=down+1  where image_id='$id'";
+		mysql_query( $sql);
+
+		//$sql_in = "insert into al_editors_voting_ip (mes_id_fk,ip_add) values ('$id','$ip')";
+		//mysql_query( $sql_in);
+	}
+
+	$result=mysql_query("select down from al_editors_votes where image_id='$id'");
+
+	$row=mysql_fetch_array($result);
+	$down_value=$row['down'];
+
+		//fw("\n\r up_value=".$up_value);
+		if ($down_value >= $limit)
+		{
+			$sql = "update wp_product_list set approved=0 where id='$id'";
+			//fw("\n\r sql=".$sql);
+			mysql_query( $sql);
+		}
+
+
+	echo $down_value;
+
+}
+
+function fw($text)
+{
+$fp = fopen('_kloplog.txt', 'a');
+fwrite($fp, $text);
+fclose($fp);
+}
+
+?>
