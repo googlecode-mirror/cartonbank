8f1d170026fdc8aec5e36fc00daab3593461a274
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2010-12-12 14:58:38 +0000

    purgatory

--- trunk/wp-content/plugins/lazyest-gallery/cartoon-banker.php
+++ trunk/wp-content/plugins/lazyest-gallery/cartoon-banker.php
@@ -10,12 +10,16 @@ Author URI: http://cartoonist.name
 function add_menu(){
 add_menu_page('Cartoon Banker','Банкир','read','lazyest-gallery/al-admin-panel.php');
 //add_submenu_page('lazyest-gallery/al-admin-panel.php','Input1', 'Отправка в базу', 'read', 'lazyest-gallery/mass-upload.php');
+add_submenu_page('lazyest-gallery/al-admin-panel.php','Прихожая', 'Прихожая', 'read', 'purgatory/purgatory.php');
 add_submenu_page('lazyest-gallery/al-admin-panel.php','Редактор базы изображений', 'Редактор базы', 'read', 'wp-shopping-cart/display-items.php');
+/*
 if (isset($current_user->wp_capabilities['editor']) && $current_user->wp_capabilities['editor']==1)
-	add_submenu_page('lazyest-gallery/al-admin-panel.php','Чистилище', 'Чистилище', 'read', 'purgatory/purgatory.php');
+	add_submenu_page('lazyest-gallery/al-admin-panel.php','Прихожая', 'Прихожая', 'read', 'purgatory/purgatory.php');
 else if (isset($current_user->wp_capabilities['administrator']) && $current_user->wp_capabilities['administrator']==1)
-	add_submenu_page('lazyest-gallery/al-admin-panel.php','Чистилище', 'Чистилище', 'read', 'purgatory/purgatory.php');
+	add_submenu_page('lazyest-gallery/al-admin-panel.php','Прихожая', 'Прихожая', 'read', 'purgatory/purgatory.php');
+*/
 }
-
+//pokazh($current_user);
+//pokazh($wpdb);
 add_action('admin_menu','add_menu');
 ?>
