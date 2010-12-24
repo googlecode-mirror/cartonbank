5794fb086b0cd62ec622408973876200db3e8c4d
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2010-12-24 14:21:13 +0000

    twitter icon

--- wp-content/themes/cartoonbank3/footer.php
+++ wp-content/themes/cartoonbank3/footer.php
@@ -1,14 +1,16 @@
-﻿<!-- footer begins -->
+<!-- footer begins -->
 <div style="clear:both;"></div>
 <div id="b"><a href="http://www.redburda.ru/" tagret=_blank><img title="Красная Бурда" src="<? echo (get_option('siteurl').'/img/b/kb88x31.gif');?>"></a>
+ <a href="http://www.spbdnevnik.ru/" tagret=_blank><img title="Петербургский дневник" src="<? echo (get_option('siteurl').'/img/b/pd88x31.gif');?>"></a>
  <a href="http://www.izvestia.ru/spb/" tagret=_blank><img title="Известия-СПб" src="<? echo (get_option('siteurl').'/img/b/iz88x31.gif');?>"></a>
- <a href="http://www.spbdnevnik.ru/" tagret=_blank><img title="Петербургский дневник" src="<? echo (get_option('siteurl').'/img/b/pd88x31.gif');?>"></a> <br>наши партнёры</div>
+ <br>наши партнёры</div>
 
 
 
 
 <div id="footer" style="background: #FFFFFF url(<? echo (get_option('siteurl').'/img/w.png');?>) top center repeat;">
-<a href="?page_id=433">Copyright</a> &copy; 2010 <a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a>
+<a href="?page_id=433">Copyright</a> &copy; 2010 <a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a><br>
+<a href="http://twitter.com/#!/cartoonbankru" target=_blank><img src="http://cartoonbank.ru/img/twitter_steps.gif" border="0"><br>следите за нами в твиттере</a>
 </div>
 
 <?php do_action('wp_footer'); ?>
