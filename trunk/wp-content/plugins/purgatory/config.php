90bc15bb9fa39f958a8b17b4e6865a2d93d1f0b1
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-01-16 20:43:20 +0000

    purgatory black mark

--- trunk/wp-content/plugins/purgatory/config.php
+++ trunk/wp-content/plugins/purgatory/config.php
@@ -7,6 +7,7 @@ $mysql_database = "cartoonbankru";
 $limit = 3;
 $limit_plus = 2;
 $limit_minus = 3;
+$limit_black = 1;
 
 $bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
 mysql_select_db($mysql_database, $bd) or die("Could not select database");
