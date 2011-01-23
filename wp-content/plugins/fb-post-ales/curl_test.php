5c84c430344d64723abb46af882571cc39cebef2
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-01-23 11:04:14 +0000

    post to facebook tests

---
+++ wp-content/plugins/fb-post-ales/curl_test.php
@@ -0,0 +1,55 @@
+﻿<?
+//STEP 1: Obtain the CODE
+/*
+$APPID = 'xxx';
+
+header("Location: https://graph.facebook.com/oauth/authorize?client_id" . urlencode($APPID) . "&redirect_uri=" . urlencode(http://www.yourdomain.com/) . "&scope=offline_access,publish_stream,manage_pages");
+*/
+?>
+
+<?
+//STEP 2: Hardcode the resulting code as $CODE (see bellow) and using the following script you can post as administrator to the page specified by $PAGE_ID:
+
+require_once('facebook/facebook.php');
+
+$APPID = '264530743602';
+$SECRET = '05e24c30ac95a33d726f6d087c3c00f4';
+$PAGE_ID = 'me';
+$CODE = '66LRNT';
+
+echo "init\n";
+$result = file_get_contents('https://graph.facebook.com/oauth/access_token?' . http_build_query(array('client_id' => $APPID, 'client_secret' => $SECRET, 'code' => $CODE, 'redirect_uri' => 'http://www.yourdomain.com/')));
+
+echo "2\n";
+
+if (preg_match('/^access_token=(.*)/', $result, $matches) !== FALSE)
+{
+	echo "3\n";
+	$access_token = $matches[1];
+
+	Facebook::$CURL_OPTS[CURLOPT_SSL_VERIFYHOST] = 0;
+	Facebook::$CURL_OPTS[CURLOPT_SSL_VERIFYPEER] = 0;
+	echo "4\n";
+	$facebook = new Facebook(array('appId' => $APPID, 'secret' => $SECRET));
+	echo "5\n";
+/*
+Fatal error: Uncaught OAuthException: An active access token must be used to query information about the current user.
+  thrown in /home/www/cb3/wp-content/plugins/fb-post-ales/facebook/facebook.php on line 543
+*/
+
+	$accounts = $facebook->api('/me/accounts', 'GET', array('access_token' => $access_token));
+	echo "6\n";
+
+	foreach ($accounts['data'] as $account)
+	{
+		if ($account['id'] == $PAGE_ID)
+		{
+			$access_token = $account['access_token'];
+		}
+	}
+		echo "7\n";
+
+	$facebook->api("/{$PAGE_ID}/feed", 'POST', array( 'message' => "It's cool man. Yeah, it's working.", 'access_token' => $access_token));
+	echo "8\n";
+}
+?>
---
+++ wp-content/plugins/fb-post-ales/index.php
@@ -0,0 +1,124 @@
+﻿<?
+https://api.facebook.com/method/stream.publish?access_token=66LRNT&id=264530743602&message=hiii&caption=My_App&description=Its_just_description.
+//190 Invalid OAuth 2.0 Access Token method stream.publish access_token 66LRNT id 264530743602 message hiii caption My_App description Its_just_description.
+
+
+https://api.facebook.com/method/stream.publish?
+access_token=66LRNT
+&id=264530743602
+&message=hiii
+&caption=My_App
+&description=Its_just_description.
+
+
+curl -F 'access_token=...' \
+     -F 'message=Hello, Arjun. I like this new API.' \
+     https://graph.facebook.com/arjun/feed
+?>
+
+<?php
+//http://www.moskjis.com/other-platforms/publish-facebook-page-wall-from-your-site
+
+define('FB_APIKEY', 'ca942722a1718dbd60cb5cc112192fab');
+define('FB_SECRET', '05e24c30ac95a33d726f6d087c3c00f4');
+define('FB_SESSION', '66LRNT');
+
+require_once('facebook/facebook.php');
+ 
+echo "post on wall";
+echo "<br/>";
+ 
+try {
+ $facebook = new Facebook(FB_APIKEY, FB_SECRET);
+ $facebook->api_client->session_key = FB_SESSION;
+ $facebook->api_client->expires = 0;
+ $message = '';
+ 
+ $attachment = array(
+ 'name' => $_POST["name"],
+ 'href' => $_POST["href"],
+ 'description' => $_POST["description"],
+ 'media' => array(array('type' => 'image',
+ 'src' => $_POST["src"],
+ 'href' => $_POST["href"])));
+ 
+ $action_links = array( array('text' => 'Visit Us', 'href' => 'cartoonbank.ru'));
+ 
+ $attachment = json_encode($attachment);
+ $action_links = json_encode($action_links);
+ 
+ $target_id = "<Target Id>";
+ $session_key = FB_SESSION;
+ 
+ if( $facebook->api('/me/feed', 'POST', $attachment)) {
+ echo "Added on FB Wall";
+ }
+
+ //if( $facebook->api_client->stream_publish($message, $attachment, $action_links, null, $target_id)) {
+ //echo "Added on FB Wall";
+ //}
+} catch(Exception $e) {
+ echo $e . "<br />";
+ }
+
+?>
+
+<?php
+exit;
+define('FB_APIKEY', 'ca942722a1718dbd60cb5cc112192fab');
+define('FB_SECRET', '05e24c30ac95a33d726f6d087c3c00f4');
+define('FB_SESSION', '66LRNT');
+require_once('facebook/facebook.php');
+echo "post on wall";
+try {
+
+	$facebook = new Facebook(FB_APIKEY, FB_SECRET);
+	$facebook->api_client->session_key = FB_SESSION;
+	$fetch =	array(	'friends' =>
+				array(	'pattern' => '.*',
+						'query' => "select uid2 from friend where uid1={$user}"));
+
+	echo $facebook->api_client->admin_setAppProperties(array('preload_fql' => json_encode($fetch)));
+//Fatal error: Call to undefined method stdClass::admin_setAppProperties() in /home/www/cb3/wp-content/plugins/fb-post-ales/index.php on line 15
+//lamp:/home/www/cb3/wp-content/plugins/fb-post-ales#
+
+	$message = 'From My App: publish steven on facebook';
+	if( $facebook->api_client->stream_publish($message))
+	echo "Added on FB Wall";
+} 
+catch(Exception $e) {
+	echo $e . "<br />";
+}
+
+?>
+
+<?php
+exit;
+
+//Generate one time session key (permanent session key)
+
+// FB_APIKEY is your facebook application api key
+
+// FB_SECRET is your application secrete key
+
+$FB_APIKEY="ca942722a1718dbd60cb5cc112192fab";
+
+$FB_SECRET="05e24c30ac95a33d726f6d087c3c00f4";
+
+$fb = new FacebookRestClient($FB_APIKEY, $FB_SECRET);
+
+$testtoken= "66LRNT"; // Replace this value with your Token Value
+
+$result = $fb->call_method('facebook.auth.getSession',
+
+array('auth_token' => $testtoken, 'generate_session_secret' => true));
+
+echo "<br /><pre>";
+
+print_r($result);
+
+echo $session_key = $result['session_key'];
+
+?>
+
+
