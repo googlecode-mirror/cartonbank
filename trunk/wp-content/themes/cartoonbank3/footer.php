65a08b7b0ae8e7817b327492d29796af930faaf9
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-01-25 10:47:09 +0000

    footer follow us

--- trunk/wp-content/themes/cartoonbank3/footer.php
+++ trunk/wp-content/themes/cartoonbank3/footer.php
@@ -6,12 +6,11 @@
  <a href="https://money.yandex.ru" target="_blank"><img src="https://money.yandex.ru/img/yamoney_logo88x31.gif " alt="Я принимаю Яндекс.Деньги" title="Я принимаю Яндекс.Деньги" border="0" width="88" height="31"/></a>
  <br>наши партнёры</div>
 
-
-
-
 <div id="footer" style="background: #FFFFFF url(<? echo (get_option('siteurl').'/img/w.png');?>) top center repeat;">
-<a href="?page_id=433">Copyright</a> &copy; 2010 <a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a><br>
-<a href="http://twitter.com/#!/cartoonbankru" target=_blank><img src="http://cartoonbank.ru/img/twitter_steps.gif" border="0"><br>следите за нами в твиттере</a>
+<a href="?page_id=433">Copyright</a> &copy; 2010-2011 <a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a><br>
+Следите за нами в <a href="http://community.livejournal.com/cartoonbank/">Живом журнале <img src="http://cartoonbank.ru/img/s_livejournal.png" title="Follow us on LiveJournal"></a> 
+<a href="http://twitter.com/#!/cartoonbankru">в Твиттере <img src="http://cartoonbank.ru/img/s_twitter.png" title="Follow us on Twitter"></a>
+<a href="http://www.facebook.com/profile.php?id=100001929470986&sk=wall">в Фейсбуке <img src="http://cartoonbank.ru/img/s_facebook.png" title="Follow us on Facebook"></a>
 </div>
 
 <?php do_action('wp_footer'); ?>
