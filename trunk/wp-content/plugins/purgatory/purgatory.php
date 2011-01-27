49c755b55948f93367e53dc62f63226516851a96
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-01-27 15:47:51 +0000

    editors comment

--- trunk/wp-content/plugins/purgatory/purgatory.php
+++ trunk/wp-content/plugins/purgatory/purgatory.php
@@ -6,9 +6,6 @@ exit;
 }
 ?>
 
-<h3>Прихожая</h3>
-
-
 <?
 include("config.php");
 
@@ -30,8 +27,8 @@ include("config.php");
 ?><br><br>
 
 
-
-<script type="text/javascript" src="http://cartoonbank.ru/wp-content/plugins/purgatory/jquery.js"></script>
+<script type="text/javascript" src="http://cartoonbank.ru/wp-includes/js/jquery/jquery.js"></script>
+<script type="text/javascript" src="http://cartoonbank.ru/wp-includes/js/jquery/jquery.form.js"></script>
 <script type="text/javascript">
 	$(function() {
 	$(".vote").click(function() 
@@ -99,6 +96,18 @@ include("config.php");
 	return false;
 		});
 
+	var options = { 
+		target:     '#divToUpdate', 
+		url:        'http://cartoonbank.ru/wp-content/plugins/purgatory/add_comment.php', 
+		success:    function() { 
+			alert('Thanks for your comment!'); 
+		} 
+	};
+
+	$('#commentform').ajaxForm(function() { 
+      alert("Thank you for your comment!");
+	  })
+
 	});
 </script>
 <script>
@@ -124,6 +133,14 @@ function sendblack(id)
 	ajax.post("http://cartoonbank.ru/wp-content/plugins/purgatory/black_vote.php", function(html){ mydiv.textContent=html;},"&id="+id);
    }
 
+function sendcomment()
+   {
+	var myelemname = "comment";
+	var mydiv = document.getElementById(myelemname);
+	ajax.post("http://cartoonbank.ru/wp-content/plugins/purgatory/add_comment.php", function(html){ mydiv.textContent=html;},"");
+   }
+
+
 </script>
 
 <style type="text/css">
@@ -132,36 +149,38 @@ function sendblack(id)
 	height:140px; border:1px dashed #29ABE2;margin-bottom:7px;
 	width:500px;
 	}
+
 	a
 	{
 	color:#DF3D82;
 	text-decoration:none;
-
 	}
+
 	a:hover
 	{
 	color:#DF3D82;
 	text-decoration:underline;
-
 	}
+
 	.up
 	{
 	padding-top: 8px;
 	height:30px; font-size:24px; text-align:center; background-color:#009900; margin-bottom:2px;
 	-moz-border-radius: 6px;-webkit-border-radius: 6px;
 	}
+
 	.up a
 	{
 	color:#FFFFFF;
 	text-decoration:none;
-
 	}
+
 	.up a:hover
 	{
 	color:#FFFFFF;
 	text-decoration:none;
-
 	}
+
 	.down
 	{
 	padding-top: 8px;
@@ -192,6 +211,7 @@ function sendblack(id)
 	color:#FFFFFF;
 	text-decoration:none;
 	}
+
 	.black a:hover
 	{
 	color:#FFFFFF;
@@ -214,20 +234,35 @@ function sendblack(id)
 	{
 	float:left; width:150px; text-align:left;
 	margin-left:10px;height:140px;margin-top:0px;
-	font-weight:bold;
+	}
 
 	.box3
 	{
+	float:left; width:270px; text-align:left;
+	margin-left:10px;height:140px;margin-top:0px;
+	font-size:0.8em;
+	}
+
+	.box4
+	{
 	float:left; width:200px; text-align:left;
 	margin-left:10px;height:140px;margin-top:0px;
-	font-weight:bold;
+	background-color:#E2FFB7;
+	}
 
+	.box5
+	{
+	float:left; width:350px; text-align:left;
+	margin-left:6px;height:800px;margin-top:0px;
+	background-color:white;
+	padding:2px;
+	}
 
 	.descr
 	{
 	clear:all; float:left; width:120px; text-align:left;
 	height:100px;
-
+	}
 
 	.img
 	{
@@ -235,11 +270,19 @@ function sendblack(id)
 	margin-left:10px;height:140px;vertical-align: top;
 	}
 
+	.c_body
+	{
+		color:#990099;
+	}
+
 	img
 	{
 	border:none;
 	}
 	</style>
+
+
+<table border=1><tr><td  valign="top">
 
 <div>
 
@@ -256,30 +299,21 @@ else
 	$sql_brand = "";
 }
 
-/*
-		$sql=mysql_query("SELECT V.image_id, V.up, V.down, V.black, P.name, P.image, B.name AS Artist, P.approved FROM al_editors_votes AS V, wp_product_list AS P, wp_product_brands AS B 
-							WHERE V.image_id=P.id 
-							AND P.brand = B.id
-							AND P.active = '1'
-							AND ((P.approved is NULL) OR (P.approved = '') OR (V.black >= '1'))".$sql_brand."
-							ORDER BY P.id DESC
-							Limit 40");
-*/
-			$sql=mysql_query("SELECT V.image_id, V.up, V.down, V.black, P.name, P.image, P.description AS Description, 
-									P.color, B.name AS Artist, P.approved, C.name as Category 
-							FROM al_editors_votes AS V, 
-							wp_product_list AS P, 
-							wp_product_brands AS B, 
-							wp_product_categories AS C,  
-							wp_item_category_associations AS A
-							WHERE V.image_id=P.id 
-							AND P.brand = B.id
-							AND P.id = A.product_id
-							AND C.id = A.category_id
-							AND P.active = '1'
-							AND ((P.approved is NULL) OR (P.approved = '') OR (V.black >= '1'))".$sql_brand."
-							ORDER BY P.id DESC
-							Limit 40");
+	$sql=mysql_query("SELECT V.image_id, V.up, V.down, V.black, P.name, P.image, P.description AS Description, 
+							P.color, B.name AS Artist, P.approved, C.name as Category 
+					FROM al_editors_votes AS V, 
+					wp_product_list AS P, 
+					wp_product_brands AS B, 
+					wp_product_categories AS C,  
+					wp_item_category_associations AS A
+					WHERE V.image_id=P.id 
+					AND P.brand = B.id
+					AND P.id = A.product_id
+					AND C.id = A.category_id
+					AND P.active = '1'
+					AND ((P.approved is NULL) OR (P.approved = '') OR (V.black >= '1'))".$sql_brand."
+					ORDER BY P.id DESC
+					Limit 40");
 
 		while($row=mysql_fetch_array($sql))
 		{
@@ -296,6 +330,18 @@ else
 		$description=nl2br(stripslashes($row['Description']));
 		$category=$row['Category'];
 		?>
+
+<?
+	$result = mysql_query("select C.comment_content, C.comment_date, U.display_name as author from wp_comments as C, wp_users as U where U.id = C.comment_author order by C.comment_date DESC LIMIT 10");
+
+	$comments_output = "";
+	while($r = mysql_fetch_array($result)) {
+		$_date = $r['comment_date'];
+		$_comment = nl2br(stripslashes($r['comment_content']));
+		$_author = $r['author'];
+		$comments_output .= "<span class='gr' title='".$_date."'>".$_author.":</span><span class='c_body'>".$_comment."</span><br>";
+	}
+?>
 
 	<div id="main">
 		<div class="box1">
@@ -315,16 +361,75 @@ else
 			<span class="gr">Название: </span><? echo ($imgname);?><br>
 			<span class="gr">Категория: </span><? echo ($category);?><br>
 			<span class="gr">Автор: </span><? echo ($artist);?><br>
-			<span class="gr">Описание: </span><? echo ($description);?>
+			<span class="gr">Описание: </span><? echo ($description);?> 	
 			<form method="post" action="http://cartoonbank.ru/wp-admin/admin.php?page=wp-shopping-cart/display-items.php"> <input type="hidden" name="edid" value="<? echo ($mes_id);?>"> <input class="borders" type="submit" value="<? echo ($mes_id);?>"> </form>
-			
 		</div>
-
 
 	</div>
 
 
 	<?php }?>
+</div>
+</td>
+<td valign="top"> 
+	<div class="box5">
+	<b>комментарии редакторов:</b>
+
+		<div id="divToUpdate" style="padding:2px;font-size:0.9em;background-color:#FFFFD7;"><? echo ($comments_output) ?></div>
+
+		<div id="commentsform">
+		<? //echo("<pre>вы пишете от юзера:".print_r($current_user->last_name,true)."</pre>"); ?>
+			<form action="http://cartoonbank.ru/wp-content/plugins/purgatory/add_comment.php" method="post" id="commentform">
+			<p>оставьте комментарий:<br>
+			<textarea name="comment" id="comment" cols="40" rows="3" tabindex="4"></textarea>
+			</p>
+		  
+			<p>
+			<input name="submit" type="submit" value="послать">
+			<input type="hidden" name="cartoon_id" value="1000">
+			<input type="hidden" name="author_id" value="<? echo ($current_user->id); ?>">
+			
+			</p>
+			<input type="hidden" id="_wp_unfiltered_html_comment" name="_wp_unfiltered_html_comment" value="5a3ab88268"><p style="display: none;"><input type="hidden" id="akismet_comment_nonce" name="akismet_comment_nonce" value="8bd460432a"></p>
+			</form>
+		</div>
 
 </div>
 
+	
+	</div>
+</td>
+</tr>
+</table>
+
+<div>
+<? //echo("<pre>current_user:".print_r($current_user->id,true)."</pre>"); ?>
+</div>
+
+
+<script type="text/javascript">
+
+	jQuery(document).ready(function($){ 
+		// bind form using ajaxForm 
+		$('#commentform').ajaxForm({ 
+			// target identifies the element(s) to update with the server response 
+			 target: '#divToUpdate', 
+	 
+			// success identifies the function to invoke when the server response 
+			// has been received; here we apply a fade-in effect to the new content 
+			success: function() { 
+				$('#divToUpdate').fadeIn('slow'); 
+			}
+			//success: successResponse
+		}); 
+	});
+
+	function successResponse(text)
+	{
+		if (text!='')
+		{
+			$('#divToUpdate').fadeIn('slow');
+		}
+	}
+
+</script>
--- trunk/wp-content/plugins/purgatory/purgatory_0.php
+++ trunk/wp-content/plugins/purgatory/purgatory_0.php
@@ -1,204 +1,287 @@
+<script type="text/javascript" src="http://cartoonbank.ru/wp-content/plugins/purgatory/jquery.js"></script>
+<script type="text/javascript" src="http://cartoonbank.ru/wp-includes/js/jquery/jquery.form.js"></script>
+
+<?
+include("config.php");
+?>
+<br><br>
+
+<script>
+
+function sendup(id)
+   {
+	var myelemname = "up"+id;
+	var mydiv = document.getElementById(myelemname);
+	ajax.post("http://cartoonbank.ru/wp-content/plugins/purgatory/up_vote.php", function(html){ mydiv.textContent=html;},"&id="+id);
+   }
+
+function senddown(id)
+   {
+	var myelemname = "down"+id;
+	var mydiv = document.getElementById(myelemname);
+	ajax.post("http://cartoonbank.ru/wp-content/plugins/purgatory/down_vote.php", function(html){ mydiv.textContent=html;},"&id="+id);
+   }
+
+function sendblack(id)
+   {
+	var myelemname = "black"+id;
+	var mydiv = document.getElementById(myelemname);
+	ajax.post("http://cartoonbank.ru/wp-content/plugins/purgatory/black_vote.php", function(html){ mydiv.textContent=html;},"&id="+id);
+   }
+
+function sendcomment()
+   {
+	var myelemname = "comment";
+	var mydiv = document.getElementById(myelemname);
+	ajax.post("http://cartoonbank.ru/wp-content/plugins/purgatory/add_comment.php", function(html){ mydiv.textContent=html;},"");
+   }
+
+
+</script>
+
+<style type="text/css">
+	#main
+	{
+	height:140px; border:1px dashed #29ABE2;margin-bottom:7px;
+	width:500px;
+	}
+	a
+	{
+	color:#DF3D82;
+	text-decoration:none;
+
+	}
+	a:hover
+	{
+	color:#DF3D82;
+	text-decoration:underline;
+
+	}
+	.up
+	{
+	padding-top: 8px;
+	height:30px; font-size:24px; text-align:center; background-color:#009900; margin-bottom:2px;
+	-moz-border-radius: 6px;-webkit-border-radius: 6px;
+	}
+	.up a
+	{
+	color:#FFFFFF;
+	text-decoration:none;
+
+	}
+	.up a:hover
+	{
+	color:#FFFFFF;
+	text-decoration:none;
+
+	}
+	.down
+	{
+	padding-top: 8px;
+	height:30px; font-size:24px; text-align:center; background-color:#cc0000; margin-top:2px;
+	-moz-border-radius: 6px;-webkit-border-radius: 6px;
+	}
+
+	.down a
+	{
+	color:#FFFFFF;
+	text-decoration:none;
+	}
+	.down a:hover
+	{
+	color:#FFFFFF;
+	text-decoration:none;
+	}
+
+	.black
+	{  
+	padding-top: 8px; color: white;
+	height:30px; font-size:24px; text-align:center; background-color:#4A4A4A; margin-top:2px;
+	-moz-border-radius: 6px;-webkit-border-radius: 6px;
+	}
+
+	.black a
+	{
+	color:#FFFFFF;
+	text-decoration:none;
+	}
+	.black a:hover
+	{
+	color:#FFFFFF;
+	text-decoration:none;
+	}
+
+	.gr
+	{
+	color:#838383;
+	text-decoration:none;
+	}
+
+	.box1
+	{
+	font-family:'Georgia', Times New Roman, Times, serif;
+	float:left; height:140px; width:50px;
+	}
+
+	.box2
+	{
+	float:left; width:150px; text-align:left;
+	margin-left:10px;height:140px;margin-top:0px;
+	font-weight:bold;
+
+	.box3
+	{
+	float:left; width:200px; text-align:left;
+	margin-left:10px;height:140px;margin-top:0px;
+	font-weight:bold;
+
+
+	.descr
+	{
+	clear:all; float:left; width:120px; text-align:left;
+	height:100px;
+
+
+	.img
+	{
+	float:left; width:200px; text-align:left;
+	margin-left:10px;height:140px;vertical-align: top;
+	}
+
+	img
+	{
+	border:none;
+	}
+	</style>
+
+<div style="font-size:1em;">
 
 <?php
-    /**
-		* o------------------------------------------------------------------------------o
-		* | This package is licensed under the Phpguru license. A quick summary is       |
-		* | that for commercial use, there is a small one-time licensing fee to pay. For |
-		* | registered charities and educational institutes there is a reduced license   |
-		* | fee available. You can read more  at:                                        |
-		* |                                                                              |
-		* |                  http://www.phpguru.org/static/license.html                  |
-		* o------------------------------------------------------------------------------o
-		*
-		* © Copyright 2008,2009 Richard Heyes
-		*/
 
-		/**
-		* This an example script that uses the datagrid and shows it off
-		*/
+if (isset($_GET['brand'])&&is_numeric($_GET['brand']))
+{
+	$brand=$_GET['brand'];
+	$sql_brand = " AND B.id = $brand ";
+}
+else
+{
+	$brand=0;
+	$sql_brand = "";
+}
+
+			$sql=mysql_query("SELECT V.image_id, V.up, V.down, V.black, P.name, P.image, P.description AS Description, 
+									P.color, B.name AS Artist, P.approved, C.name as Category 
+							FROM al_editors_votes AS V, 
+							wp_product_list AS P, 
+							wp_product_brands AS B, 
+							wp_product_categories AS C,  
+							wp_item_category_associations AS A
+							WHERE V.image_id=P.id 
+							AND P.brand = B.id
+							AND P.id = A.product_id
+							AND C.id = A.category_id
+							AND P.active = '1'
+							AND ((P.approved is NULL) OR (P.approved = '') OR (V.black >= '1'))".$sql_brand."
+							ORDER BY P.id DESC
+							Limit 40");
+
+		while($row=mysql_fetch_array($sql))
+		{
+		$msg=$row['image_id'];
+		$mes_id=$row['image_id'];
+		$up=$row['up'];
+		$down=$row['down'];
+		$black=$row['black'];
+		$img=$row['image'];
+		$imgpath = "http://cartoonbank.ru/wp-content/plugins/wp-shopping-cart/images/".$img; 
+		$previewpath = "http://cartoonbank.ru/wp-content/plugins/wp-shopping-cart/product_images/".$img;
+		$imgname=nl2br(stripslashes($row['name']));
+		$artist=$row['Artist'];
+		$description=nl2br(stripslashes($row['Description']));
+		$category=$row['Category'];
+		?>
+
+	<div id="main">
+		<div class="box1">
+			<div class='up'><a href="" id="up<?php echo $mes_id;?>"  onclick="sendup(<?php echo $mes_id; ?>);return false;" class="vote" id="<?php echo $mes_id; ?>" name="up"><?php echo $up; ?></a></div>
+			<div class='down'><a href="" id="down<?php echo $mes_id;?>" onclick="senddown(<?php echo $mes_id; ?>);return false;" class="vote" id="<?php echo $mes_id; ?>" name="down"><?php echo $down; ?></a></div>
+			<div class='black'><a href="" id="black<?php echo $mes_id;?>" onclick="sendblack(<?php echo $mes_id; ?>);return false;" class="vote" id="<?php echo $mes_id; ?>" name="black"><?php echo $black; ?></a></div>
+		</div>
+
+		<div class='box2' >
+			<?php //echo $row['image_id']; ?>
+			<div class="img">
+				<a href="<? echo ($previewpath); ?>" target=_blank><img src="<? echo ($imgpath); ?>"></a>
+			</div>
+		</div>
+
+		<div class="box3">
+			<span class="gr">Название: </span><? echo ($imgname);?><br>
+			<span class="gr">Категория: </span><? echo ($category);?><br>
+			<span class="gr">Автор: </span><? echo ($artist);?><br>
+			<span class="gr">Описание: </span><? echo ($description);?> 	
+			<form method="post" action="http://cartoonbank.ru/wp-admin/admin.php?page=wp-shopping-cart/display-items.php"> <input type="hidden" name="edid" value="<? echo ($mes_id);?>"> <input class="borders" type="submit" value="<? echo ($mes_id);?>"> </form>
+		</div>
+
+
+	</div>
+
+
+	<?php }?>
+</div>
+
+
+<div id="divToUpdate" style="width:800px;height:200px;font-size:0.6em;background-color:pink;">...</div>
+		<div id="commentsform">
+		.<br>
+
+			<form action="http://cartoonbank.ru/wp-content/plugins/purgatory/add_comment.php" method="post" id="commentform">
+			<p>текст:<br>
+			<textarea name="comment" id="comment" cols="90" rows="3" tabindex="4"></textarea>
+			</p>
+		  
+			<p>
+			<input name="submit" type="submit" value="послать">
+			<input type="hidden" name="cartoon_id" value="1000">
+			<input type="hidden" name="author_id" value="<? echo ($current_user->id); ?>">
+			
+			</p>
+			<input type="hidden" id="_wp_unfiltered_html_comment" name="_wp_unfiltered_html_comment" value="5a3ab88268"><p style="display: none;"><input type="hidden" id="akismet_comment_nonce" name="akismet_comment_nonce" value="8bd460432a"></p>
+			
+			</form>
+		</div>
+
+
+
+</div>
+
+
+<script type="text/javascript">
+
+	jQuery(document).ready(function($){ 
+		// bind form using ajaxForm 
+		$('#commentform').ajaxForm({ 
+			// target identifies the element(s) to update with the server response 
+			 target: '#divToUpdate', 
+	 
+			// success identifies the function to invoke when the server response 
+			// has been received; here we apply a fade-in effect to the new content 
+			success: function() { 
+				$('#divToUpdate').fadeIn('slow'); 
+			}
+			//success: successResponse
+		}); 
+	});
+
+	function successResponse(responseJson)
+	{
+		alert (responseJson);
+		var data = jQuery.parseJSON(responseJson);
+		alert(data);
+		alert(data[1][1]);
 		
-		/**
-		* Include the datagrid code 
-		*/
+		$('#divToUpdate').fadeIn('slow');
+	}
 
-	//pokazh(ABSPATH.'/wp-content/RGrid/RGrid.php'); //Z:\home\localhost\www//wp-content/RGrid/RGrid.php
+</script>
 
-    require_once(WP_CONTENT_DIR.'/RGrid/RGrid.php');
-    
-    $params['hostname'] = 'localhost';
-    $params['username'] = 'z58365_cbru3';
-    $params['password'] = 'greenbat';
-    $params['database'] = 'cartoonbankru';
-
-	// all not approved pics
-    $sql = "SELECT wp_product_list.id as ID, wp_product_list.image AS image, wp_product_list.name AS title, wp_product_brands.name AS author, COUNT(V.vote_id) AS votes, SUM(V.vote_value) AS points
-FROM wp_product_list, wp_product_brands, al_editors_votes as V
-WHERE wp_product_list.brand = wp_product_brands.id
-	AND V.image_id = wp_product_list.id
-	AND wp_product_list.active =1
-	AND wp_product_list.visible =1
-	AND wp_product_list.approved = False";
-//pokazh ($sql);
-//$sql = "SELECT * FROM `wp_fsr_post` as p, `wp_fsr_user` as u WHERE p.id = u.post order by vote_date desc";
-
-    /**
-    * Create the datagrid with the connection parameters and SQL query
-    * defined above. You MUST specify an ORDER BY
-    * clause (with ASC/DESC direction indicator) - if not then ordering
-    * will be fubar. I will eventually fix this so that the headers
-    * aren't clickable if you don't supply an ORDER BY, for the time
-    * being just specify an ORDER BY clause (the one you specify will
-    * be used by default).
-    */
-    $grid = RGrid::Create($params, $sql);
-
-    /**
-    * Disable sorting
-    */
-    //$grid->allowSorting = false;
-        
-    /**
-    * Turn the column headers off/on
-    */
-    $grid->showHeaders = true;
-    
-    /**
-    * Different to the above - this is the HTML that is shown above the
-    * actual datagrid
-    */
-    $grid->SetHeaderHTML('<div style="text-align: center">Статистика голосования</div>');
-
-    /**
-    * Sets nice(r) display names instead of the raw column names
-    */
-    $grid->SetDisplayNames(array('ID'       => '№',
-                                 'votes'   => 'голоса',
-                                 'points'    => 'баллы',
-                                 'user' => 'user',
-                                 'vote_date'   => 'проголосовано',
-                                 'post'   => 'post',
-                                 'title'   => 'название',
-                                 'image'   => 'иконка',
-                                 'author'   => 'автор',
-                                 'average'   => 'средн.',
-                                 'ip'    => 'ip'));
-
-    /**
-    * This hides a column from the result set. It would of course be easier and
-    * faster just to not select it, but this isn't always possible.
-    */
-	#$grid->HideColumn('ID');
-                               
-    /**
-    * This simply sets the specified columns not to be passed through htmlspecialchars()
-    * Generally any column that shows HTML
-    */
-    $grid->NoSpecialChars('actions', 'ac_id');
-    $grid->NoSpecialChars('ID', 'image','title','5');
-    
-    /**
-    * A callback function that gets called for each and every row.
-    * The callback function should take a single argument by reference,
-    * which is an associative array of the row data. It's by reference
-    * so that any changes you make will not be lost.
-    */
-    $grid->rowcallback = 'RowCallback';
-
-    function RowCallback(&$row)
-    {
-		pokazh($row);
-		$row['image'] = "<a href='?page_id=29&cartoonid=".$row['ID']."'><img src='".get_option('siteurl')."/wp-content/plugins/wp-shopping-cart/images/".$row['image']."' width='140' height='140' style='border:1px solid black;margin:1px;'></a>";
-
-		$row['5'] = '<input type="button" value="Yes" > <input type="button" value="No" >';
-		//$row['ID'] = "<a href='?page_id=29&cartoonid=".$row['ID']."'>".$row['ID']."</a>";
-		//$row['title'] = nl2br(stripslashes($row['title']));
-		//$row['average'] = round($row['average'],2);
-    }
-
-	/**
-    * Here just for show. This function sets the number of rows to set per page.
-    * The default is 20.
-    */
-    $grid->SetPerPage(20);
-    
-    /**
-		* Again, this is purely here for show. It doesn't do anything, but simply shows
-		* how you can use this function. If you change the perPage after you call
-		* this function, then obviously the page count will change.
-		*/
-		// echo '<p>Number of pages: ' . $grid->GetPageCount() . '<br />';
-		
-		/**
-		* Again, here just for show. Gets the number of rows in the grid.
-		*/
-		// echo 'Number of rows: ' . $grid->GetRowCount() . '</p>';
-		
-		/**
-		* Gets the connection resource. Here just for show.
-		*/
-		//echo 'The connection (var_dump()): ';
-		//var_dump($grid->GetConnection());
-
-		/**
-		* Gets the result resource. Here just for show.
-		*/
-		//echo '<br />The result resouce (var_dump()): ';
-		//var_dump($grid->GetResultset());
-
-		/**
-		* The HTML. The appearance can be customised using CSS. Naturally you would (should) put
-		* all of the datagrid styling in a central CSS file that can be <link>ed to by all of
-		* your websites' pages. That way:
-		*  o All of your datagrids will look the same
-		*  o Changes to the appearance will affect all of your datagrids across your
-		*    entire website. You might want this necessarily, but must do.
-		*
-		* eg. <link rel="stylesheet" type="text/css" media="screen" href="/css/datagrid.css" />
-		*/
-	?>
-
-    <style type="text/css">
-    <!--
-        table.datagrid {
-            font-family: Arial;
-        }
-        .datagrid thead th#header {
-            background-color: #ddd;
-        }
-        .datagrid thead th {
-            color: black;
-            border-bottom: 1px solid #DBDBDB;
-            font-size: 8pt;
-            font-family: Verdana;
-            text-align: center;
-			padding:10px;
-			margin:0px;
-        }
-        .datagrid thead th#header {
-            border: 0;
-        }
-        .datagrid tbody td.altrow {
-            background-color: #fff;
-        }
-        .datagrid tbody td.altcol
-        {
-          background-color: #eee;
-        }
-        .datagrid tbody td {
-            padding-left: 5px;
-			font-size: 8pt;
-			border-bottom: 1px solid #DBDBDB;
-			vertical-align:middle;
-			text-align:center;
-        }
-        .datagrid a {
-            text-decoration: none;
-        }
-    // -->
-    </style>
-
-	<?php $grid->Display() ?>
-    
-    <?if(preg_match('/^\/datagrid/', $_SERVER['REQUEST_URI'])):?>
-        <pre><?print_r($grid)?></pre>
-    <?endif?>
