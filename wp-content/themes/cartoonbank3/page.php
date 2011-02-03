784e8377fbf0a14d09b9d4f6c92185dea73281d0
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-02-03 11:29:43 +0000

    header and footer + UI tweaks

--- wp-content/themes/cartoonbank3/page.php
+++ wp-content/themes/cartoonbank3/page.php
@@ -29,7 +29,7 @@ else
 </div>
 
 	<div style="clear:both;"></div>
-	<div id="topfooter" style="border-top:1px solid #C0C0C0; background: #FFFFFF url(<? echo (get_option('siteurl').'/img/w.png');?>) top center repeat-y;"><b>Популярные темы:</b>
+	<div id="topfooter" style="padding-top:10px;border-top:1px solid #C0C0C0; background: #FFFFFF url(<? echo (get_option('siteurl').'/img/w.png');?>) top center repeat-y;"><h1>Популярные темы:</h1>
 	<?
 		$filepath = WP_CONTENT_DIR . '/tags/ales-tag_cloud_small.php' ;
 		if ( file_exists( $filepath ) )
