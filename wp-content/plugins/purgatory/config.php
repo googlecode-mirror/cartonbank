c965b1b94c276965da667afb00ad90d36ca2cafe
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-03-06 20:39:44 +0000

    theme of the day purgatory config

--- wp-content/plugins/purgatory/config.php
+++ wp-content/plugins/purgatory/config.php
@@ -4,9 +4,9 @@ $mysql_user = "z58365_cbru3";
 $mysql_password = "greenbat";
 $mysql_database = "cartoonbankru";
 
-$limit = 3;
-$limit_plus = 2;
-$limit_minus = 3;
+$limit = 4;
+$limit_plus = 4;
+$limit_minus = 4;
 $limit_black = 1;
 
 $bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
