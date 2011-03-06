7c3b29d800002615a80b2d47fa6a9b4fe65d34ce
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-03-06 20:39:44 +0000

    theme of the day purgatory config

--- trunk/wp-content/plugins/lazyest-gallery/cartoon-banker.php
+++ trunk/wp-content/plugins/lazyest-gallery/cartoon-banker.php
@@ -20,6 +20,13 @@ if (isset($current_user->wp_capabilities['editor']) && ($current_user->wp_capabi
 if (isset($current_user->wp_capabilities['administrator']) && ($current_user->wp_capabilities['administrator']==1))
 	add_submenu_page('lazyest-gallery/al-admin-panel.php','Прихожая', 'Прихожая', 'read', 'purgatory/purgatory.php');
 
+if (isset($current_user->wp_capabilities['administrator']) && ($current_user->wp_capabilities['administrator']==1))
+	add_submenu_page('lazyest-gallery/al-admin-panel.php','Тема дня', 'Тема дня', 'read', 'wp-shopping-cart/themeoftheday.php');
+
+if (isset($current_user->wp_capabilities['editor']) && ($current_user->wp_capabilities['editor']==1))
+	add_submenu_page('lazyest-gallery/al-admin-panel.php','Тема дня', 'Тема дня', 'read', 'wp-shopping-cart/themeoftheday.php');
+
+
 
 //add_submenu_page('lazyest-gallery/al-admin-panel.php','Прихожая', 'Прихожая', 'read', 'purgatory/purgatory.php');
 add_submenu_page('lazyest-gallery/al-admin-panel.php','Редактор базы изображений', 'Редактор базы', 'read', 'wp-shopping-cart/display-items.php');
