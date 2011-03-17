ecae4518e96491a880fe131724a14a2b0a5cd01b
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-03-17 18:51:58 +0000

    rating

--- wp-content/plugins/wp-shopping-cart/homepage_products_functions.php
+++ wp-content/plugins/wp-shopping-cart/homepage_products_functions.php
@@ -134,80 +134,80 @@ function top_votes($content = '')
 	}
 
 $_order_filter = " ORDER BY rate DESC";
-$_order_description = "Отсортировано по рейтингу (произведению среднего балла и корня четвёртой степени из количества голосов). По убыванию.";
+$_order_description = "Сортировка по рейтингу (произведению среднего балла и корня четвёртой степени из количества голосов). По убыванию.";
 
 switch ($_order){
 	case 11:
 		$_order_filter = " ORDER BY id ASC";
-		$_order_description = "Отсортировано по регистрационному номеру. По возрастанию.";
+		$_order_description = "Сортировка по регистрационному номеру. По возрастанию.";
 		break;
 	case 12:
 		$_order_filter = " ORDER BY id DESC";
-		$_order_description = "Отсортировано по регистрационному номеру. По убыванию.";
+		$_order_description = "Сортировка по регистрационному номеру. По убыванию.";
 		break;
 	case 21:
 		$_order_filter = " ORDER BY image ASC";
-		$_order_description = "Отсортировано по файлу изображения. По возрастанию.";
+		$_order_description = "Сортировка по файлу изображения. По возрастанию.";
 		break;
 	case 22:
 		$_order_filter = " ORDER BY image DESC";
-		$_order_description = "Отсортировано по файлу изображения. По убыванию.";
+		$_order_description = "Сортировка по файлу изображения. По убыванию.";
 		break;
 	case 31:
 		$_order_filter = " ORDER BY title ASC";
-		$_order_description = "Отсортировано по названию рисунка. По возрастанию.";
+		$_order_description = "Сортировка по названию рисунка. По возрастанию.";
 		break;
 	case 32:
 		$_order_filter = " ORDER BY title DESC";
-		$_order_description = "Отсортировано по названию рисунка. По убыванию.";
+		$_order_description = "Сортировка по названию рисунка. По убыванию.";
 		break;
 	case 41:
 		$_order_filter = " ORDER BY author ASC";
-		$_order_description = "Отсортировано по имени автора. По возрастанию.";
+		$_order_description = "Сортировка по имени автора. По возрастанию.";
 		break;
 	case 42:
 		$_order_filter = " ORDER BY author DESC";
-		$_order_description = "Отсортировано по имени автора. По убыванию.";
+		$_order_description = "Сортировка по имени автора. По убыванию.";
 		break;
 	case 51:
 		$_order_filter = " ORDER BY votes ASC";
-		$_order_description = "Отсортировано по количеству голосов. По возрастанию.";
+		$_order_description = "Сортировка по количеству голосов. По возрастанию.";
 		break;
 	case 52:
 		$_order_filter = " ORDER BY votes DESC";
-		$_order_description = "Отсортировано по количеству голосов. По убыванию.";
+		$_order_description = "Сортировка по количеству голосов. По убыванию.";
 		break;
 	case 61:
 		$_order_filter = " ORDER BY points ASC";
-		$_order_description = "Отсортировано по количеству баллов. По возрастанию.";
+		$_order_description = "Сортировка по количеству баллов. По возрастанию.";
 		break;
 	case 62:
 		$_order_filter = " ORDER BY points DESC";
-		$_order_description = "Отсортировано по количеству баллов. По убыванию.";
+		$_order_description = "Сортировка по количеству баллов. По убыванию.";
 		break;
 	case 71:
 		$_order_filter = " ORDER BY rate ASC";
-		$_order_description = "Отсортировано по рейтигу (произведению среднего балла и корня четвёртой степени из количества голосов). По возрастанию.";
+		$_order_description = "Сортировка по рейтингу (произведению среднего балла и корня четвёртой степени из количества голосов). По возрастанию.";
 		break;
 	case 72:
 		$_order_filter = " ORDER BY rate DESC";
-		$_order_description = "Отсортировано по рейтингу (произведению среднего балла и корня четвёртой степени из количества голосов). По убыванию.";
+		$_order_description = "Сортировка по рейтингу (произведению среднего балла и корня четвёртой степени из количества голосов). По убыванию.";
 		break;
 	case 81:
 		$_order_filter = " ORDER BY average ASC, points DESC";
-		$_order_description = "Отсортировано по среднему баллу. По возрастанию.";
+		$_order_description = "Сортировка по среднему баллу. По возрастанию.";
 		break;
 	case 82:
 		$_order_filter = " ORDER BY average DESC, points DESC ";
-		$_order_description = "Отсортировано по среднему баллу. По убыванию.";
+		$_order_description = "Сортировка по среднему баллу. По убыванию.";
 		break;
 	case 91:
 		$_order_filter = " ORDER BY vote_date ASC";
-		$_order_description = "Отсортировано по дате голосования. По возрастанию.";
+		$_order_description = "Сортировка по дате голосования. От старых к новым.";
 		break;
 	case 92:
 		$_order_filter = " ORDER BY vote_date DESC";
-		$_order_description = "Отсортировано по дате голосования. По убыванию.";
+		$_order_description = "Сортировка по дате голосования. От новых к старым.";
 		break;
 }
 
@@ -218,7 +218,7 @@ $sql = "SELECT
 		wp_product_brands.name AS author, 
 		COUNT(*) AS votes, 
 		SUM(wp_fsr_user.points) AS points, 
-		AVG(points)*SQRT(COUNT(*)) AS rate, 
+		AVG(points)*SQRT(SQRT(COUNT(*))) AS rate, 
 		AVG(points) as average, 
 		vote_date  
 			FROM wp_fsr_user, wp_product_list, wp_product_brands 
@@ -247,12 +247,12 @@ $sql = "SELECT
 			  {
 				$output .= "<div><h1>".$product_list[0]['author'].".";
 			  }
-			  $output .= " Сто лучших работ</h1><div style='color:white;background-color:#668bb7;padding:2px;font-weight:bold;display:block;'>".$_order_description."</div><div style='color:#818181;'>Сортировать 
-			  по дате последнего голосования: <a href='".$pageURL."&ord=91&br=".$_br."'>старые впереди</a>, <a href='".$pageURL."&ord=92&br=".$_br."'>новые впереди</a>; 
-			  по среднему баллу: <a href='".$pageURL."&ord=81&br=".$_br."'>по возрастанию</a>, <a href='".$pageURL."&ord=82&br=".$_br."'>по убыванию</a>; 
-			  по количеству баллов: <a href='".$pageURL."&ord=61&br=".$_br."'>по возрастанию</a>, <a href='".$pageURL."&ord=62&br=".$_br."'>по убыванию</a>; 
-			  по количеству голосов: <a href='".$pageURL."&ord=51&br=".$_br."'>по возрастанию</a>, <a href='".$pageURL."&ord=52&br=".$_br."'>по убыванию</a>; 
-			  по рейтингу: <a href='".$pageURL."&ord=71&br=".$_br."'>по возрастанию</a>, <a href='".$pageURL."&ord=72&br=".$_br."'>по убыванию</a>;
+			  $output .= " Сто лучших работ</h1><div style='color:white;background-color:#668bb7;padding:2px;padding-left:6px;font-weight:bold;display:block;'>".$_order_description."</div><div style='color:#818181;padding-left:150px;'>Сортировать 
+			  <b>по дате</b> последнего голосования: <a style='background-color:#FFDFFA;' href='".$pageURL."&ord=91&br=".$_br."'>старые впереди</a>, <a style='background-color:#DFFFEF;' href='".$pageURL."&ord=92&br=".$_br."'>новые впереди</a>; 
+			  <br><b>по среднему баллу</b>: <a style='background-color:#FFDFFA;' href='".$pageURL."&ord=81&br=".$_br."'>по возрастанию</a>, <a style='background-color:#DFFFEF;' href='".$pageURL."&ord=82&br=".$_br."'>по убыванию</a>; 
+			  <br><b>по количеству баллов</b>: <a style='background-color:#FFDFFA;' href='".$pageURL."&ord=61&br=".$_br."'>по возрастанию</a>, <a style='background-color:#DFFFEF;' href='".$pageURL."&ord=62&br=".$_br."'>по убыванию</a>; 
+			  <br><b>по количеству голосов</b>: <a style='background-color:#FFDFFA;' href='".$pageURL."&ord=51&br=".$_br."'>по возрастанию</a>, <a style='background-color:#DFFFEF;' href='".$pageURL."&ord=52&br=".$_br."'>по убыванию</a>; 
+			  <br><b>по рейтингу</b>: <a style='background-color:#FFDFFA;' href='".$pageURL."&ord=71&br=".$_br."'>по возрастанию</a>, <a style='background-color:#DFFFEF;' href='".$pageURL."&ord=72&br=".$_br."'>по убыванию</a>;
 			  </div></div>";
 		  }
 
@@ -263,7 +263,7 @@ $sql = "SELECT
 		if($product['image'] != '')
 		  {
 		  //$output .= "<img src='$siteurl/wp-content/plugins/wp-shopping-cart/product_images/".$product['image']."' title='".$product['name']."' alt='".$product['name']."' />\n\r";
-		  $output .= "<img src='$siteurl/wp-content/plugins/wp-shopping-cart/images/".$product['image']."' title='".$product['author'].". &quot;".$product['title']."&quot;. Голосов: ".$product['votes'].". Баллов: " . $product['points'] . ". Рейт: " . round($product['average'],3) . "' class='thumb'/>";
+		  $output .= "<img src='$siteurl/wp-content/plugins/wp-shopping-cart/images/".$product['image']."' title='".$product['author'].". &quot;".$product['title']."&quot;. Голосов: ".$product['votes'].". Баллов: " . $product['points'] . ". Средний балл: " . round($product['average'],3) .". Рейт: " . round($product['rate'],3) . "' class='thumb'/>";
 		  }
 		$output .= "</a>";
 		$output .= "</div>\n\r";
