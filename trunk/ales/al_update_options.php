a42ed48390a274d3f0b6b8dfc20adc4bb150b384
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-03-27 19:59:12 +0000

    totals calculation

--- trunk/ales/al_update_options.php
+++ trunk/ales/al_update_options.php
@@ -12,7 +12,7 @@ Update total cartoon number
 	//insert into wp_options ('option_name') values ('total_cartoons_to_show')
 
 
-		$sql = "SELECT COUNT(*) as count FROM `wp_product_list`,`wp_item_category_associations` WHERE `wp_product_list`.`active`='1'  AND `wp_item_category_associations`.`category_id` != '666' AND `wp_item_category_associations`.`category_id` != '777'   AND `wp_product_list`.`approved` = '1'  AND `wp_product_list`.`visible`='1' AND `wp_product_list`.`brand` in (SELECT DISTINCT id FROM `wp_product_brands`) AND `wp_product_list`.`id` = `wp_item_category_associations`.`product_id`";
+		$sql = "SELECT COUNT(*) as count FROM `wp_product_list`,`wp_item_category_associations` WHERE `wp_product_list`.`active`='1' AND `wp_item_category_associations`.`category_id` != '777'   AND `wp_product_list`.`approved` = '1'  AND `wp_product_list`.`visible`='1' AND `wp_product_list`.`brand` in (SELECT DISTINCT id FROM `wp_product_brands`) AND `wp_product_list`.`id` = `wp_item_category_associations`.`product_id`";
 
 		if (!($result = mysql_query($sql))) {die('Invalid query: ' . mysql_error());}
 
