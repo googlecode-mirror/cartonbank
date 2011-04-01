edc6b7154bc828e19c4671875c777eb6a59accd3
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-04-01 10:09:13 +0000

    header doctype

--- trunk/wp-content/themes/cartoonbank3/header.php
+++ trunk/wp-content/themes/cartoonbank3/header.php
@@ -9,7 +9,7 @@ if ($_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'] == "cartoonbank.ru/?page_id=29
 
 // <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 // <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>
-<!DOCTYPE html />
+<!doctype html>
 <head profile="http://gmpg.org/xfn/11" />
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 <meta name="distribution" content="global" />
