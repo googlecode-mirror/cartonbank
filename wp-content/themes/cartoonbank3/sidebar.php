a0960ec558f0d64548c10e90a4f66812de5873be
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-04-09 15:38:07 +0000

    advanced search

--- wp-content/themes/cartoonbank3/sidebar.php
+++ wp-content/themes/cartoonbank3/sidebar.php
@@ -29,9 +29,9 @@ if (isset($_GET['brand']) && is_numeric($_GET['brand']))
 
 	<h2>Поиск</h2>
 	<form method="post" id="searchform" action="?page_id=29">
-	<input id="s" size="25" type="text" value="введите поисковое слово..." name="cs" onfocus="this.value='';"/>
+	<input id="s" size="25" type="text" value="введите поисковое слово..." name="cs" onfocus="this.value='';"/><br />
 	<input type="hidden" id="brand" name="brand" value="<?echo $brandid;?>" />
-	<input type="submit" id="searchsubmit" class='borders' value="Искать" />
+	<input type="submit" id="searchsubmit" class='borders' value="Искать" style="margin-top:2px;" />
 	</form>
 	<a href="?page_id=927">расширенный поиск</a>
 
