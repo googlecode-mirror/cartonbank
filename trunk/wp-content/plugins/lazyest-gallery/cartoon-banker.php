081d9e816b5c023123929a3201538d24032d7653
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-04-11 08:18:07 +0000

    new menu item allsales

--- trunk/wp-content/plugins/lazyest-gallery/cartoon-banker.php
+++ trunk/wp-content/plugins/lazyest-gallery/cartoon-banker.php
@@ -27,6 +27,15 @@ if (isset($current_user->wp_capabilities['editor']) && ($current_user->wp_capabi
 	add_submenu_page('lazyest-gallery/al-admin-panel.php','Тема дня', 'Тема дня', 'read', 'wp-shopping-cart/themeoftheday.php');
 
 
+if (isset($current_user->wp_capabilities['administrator']) && ($current_user->wp_capabilities['administrator']==1))
+	add_submenu_page('lazyest-gallery/al-admin-panel.php','Продажи работ', 'Продажи работ', 'read', 'wp-shopping-cart/allsales.php');
+
+if (isset($current_user->wp_capabilities['editor']) && ($current_user->wp_capabilities['editor']==1))
+	add_submenu_page('lazyest-gallery/al-admin-panel.php','Продажи работ', 'Продажи работ', 'read', 'wp-shopping-cart/allsales.php');
+
+if (isset($current_user->wp_capabilities['author']) && ($current_user->wp_capabilities['author']==1))
+	add_submenu_page('lazyest-gallery/al-admin-panel.php','Продажи работ', 'Продажи работ', 'read', 'wp-shopping-cart/allsales.php');
+
 
 //add_submenu_page('lazyest-gallery/al-admin-panel.php','Прихожая', 'Прихожая', 'read', 'purgatory/purgatory.php');
 add_submenu_page('lazyest-gallery/al-admin-panel.php','Редактор базы изображений', 'Редактор базы', 'read', 'wp-shopping-cart/display-items.php');
