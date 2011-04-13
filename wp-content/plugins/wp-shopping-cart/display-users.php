ccfb342a5865c1242b5b2fdf1c724e1b75d62737
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-04-13 16:19:43 +0000

    User list

--- wp-content/plugins/wp-shopping-cart/display-users.php
+++ wp-content/plugins/wp-shopping-cart/display-users.php
@@ -7,7 +7,7 @@ $product_list = $wpdb->get_results($sql,ARRAY_A) ;
   <h2>Юзеры</h2>
 
 <?php
-echo "        <table id='itemlist' style='padding:4px;background-color:silver;'>\n\r";
+echo "        <table id='itemlist' style='padding:4px;'>\n\r";
 
 echo "          <tr class='firstrow' style='padding:2px; border:1px solid black; background-color:#c0c0c0;'>\n\r";
 echo "            <td>\n\r";
@@ -50,7 +50,7 @@ if($product_list != null)
   {
   foreach($product_list as $product)
 	{
-		echo "          <tr style='padding:2px; background-color:white;'>\n\r";
+		echo "          <tr style='padding:2px;border-bottom:1px solid silver'>\n\r";
 		echo "            <td>\n\r<a href='". get_option('siteurl')."/wp-admin/user-edit.php?user_id=".$product['id']."'>";
 		echo $product['id'];
 		echo "</a>            </td>\n\r";
