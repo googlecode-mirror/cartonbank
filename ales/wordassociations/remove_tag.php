7ea3508257cdf39793766fae8b517cde137c9e32
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-04-14 14:44:47 +0000

    tags removal

---
+++ ales/wordassociations/remove_tag.php
@@ -0,0 +1,79 @@
+<?
+include("/home/www/cb/ales/config.php");
+global $imagepath;
+
+if(isset($_GET['wrd']) && $_GET['wrd']!='' && isset($_GET['id']) && is_numeric($_GET['id']))
+{
+	$id = $_GET['id'];
+	$wrd = trim($_GET['wrd']);
+
+	// update database
+	$sql = "select additional_description from wp_product_list where id=".$id." LIMIT 1";
+	
+		$link = mysql_connect($mysql_hostname, $mysql_user, $mysql_password);
+		mysql_set_charset('utf8',$link);
+	
+		$result = mysql_query($sql);
+		if (!$result) {die('Invalid query: ' . mysql_error());}
+
+	$product=mysql_fetch_array($result);
+	$_tags = $product['additional_description'];
+	
+	$_tags_array = explode(',',$_tags);
+
+	$_remove_this_one = '';
+		foreach ($_tags_array as $key => $value)
+		{
+			$word = trim($value);
+
+			fw("word: [".$word."]");
+
+
+			if ($wrd==$word)
+			{
+				fw("found: [".$word."]");
+
+				$_remove_this_one = $key;
+			}
+		}
+
+fw("_remove_this_one=".$_remove_this_one);
+
+	if ($_remove_this_one!='')
+	{
+		unset($_tags_array[$_remove_this_one]);
+		$_tags_array = array_values($_tags_array);
+		var_dump($_tags_array);
+	}
+
+	$original_tags = implode(", ",$_tags_array);
+	$original_tags = str_replace("  ", " ", $original_tags);
+
+fw("original_tags: ".$original_tags);
+
+/*
+	$original_tags = str_replace(",", ", ", $original_tags);
+	$original_tags = str_replace("  ", " ", $original_tags);
+	$original_tags = str_replace($wrd, "", $original_tags);
+*/	
+	$sql = "update wp_product_list set additional_description = '".$original_tags."' where id=".$id;
+
+fw($sql);
+
+	$result = mysql_query($sql);
+
+		if (!$result) {die('Invalid query: ' . mysql_error());}
+		mysql_close($link);
+}
+
+return;
+
+function fw($text)
+{
+	$fp = fopen('_kloplog.txt', 'a');
+	fwrite($fp, "\r\n");
+	fwrite($fp, $text);
+	fclose($fp);
+}
+
+?>
