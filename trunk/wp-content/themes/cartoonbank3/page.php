ae300452c952dfabe68048c282bd810c64abd0b1
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-04-22 15:52:27 +0000

    footer design

--- trunk/wp-content/themes/cartoonbank3/page.php
+++ trunk/wp-content/themes/cartoonbank3/page.php
@@ -29,13 +29,14 @@ else
 </div>
 
 	<div style="clear:both;"></div>
-	<div id="topfooter" style="padding-top:10px;border-top:1px solid #C0C0C0; background: #FFFFFF url(<? echo (get_option('siteurl').'/img/w.png');?>) top center repeat-y;"><h1>Популярные темы:</h1>
+	<div id="topfooter" style="padding-top:10px;border-top:1px solid #C0C0C0; background: #FFFFFF url(<? echo (get_option('siteurl').'/img/w.png');?>) top center repeat-y;"><h1>Популярные темы. Кликайте или <a href="http://cartoonbank.ru/?page_id=927" style="color:#CC3300;border:1px solid silver;padding-left:6px;padding-right:6px;">введите своё ключевое слово</a></h1>
 	<?
 		$filepath = WP_CONTENT_DIR . '/tags/ales-tag_cloud_small.php' ;
 		if ( file_exists( $filepath ) )
 		require_once( WP_CONTENT_DIR . '/tags/ales-tag_cloud_small.php' );
 	?>
 	</div>
+	<div style="text-align:right;background-color:white;border-bottom:1px solid #C0C0C0;"><a href="http://cartoonbank.ru/?page_id=390"><h1>...здесь ещё больше ключевых слов</h1></a></div>
 
 </div>
 <!-- The main column ends  -->
