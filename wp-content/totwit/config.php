b6013783fccfd5b0dcad96b3285ac23b22608cd3
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-04-25 06:35:26 +0000

    totwit

---
+++ wp-content/totwit/config.php
@@ -0,0 +1,10 @@
+<?php
+$mysql_hostname = "localhost";
+$mysql_user = "z58365_cbru3";
+$mysql_password = "greenbat";
+$mysql_database = "cartoonbankru";
+
+$bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect to the database");
+mysql_select_db($mysql_database, $bd) or die("Could not select database");
+
+?>
