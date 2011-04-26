a9c0260ad54bfa46141f63246eebd5f9688798be
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-04-26 09:52:20 +0000

    utf-8 BOM removed

--- trunk/wp-content/plugins/purgatory/config.php
+++ trunk/wp-content/plugins/purgatory/config.php
@@ -1,15 +1,12 @@
-﻿<?php
+<?php
 $mysql_hostname = "localhost";
 $mysql_user = "z58365_cbru3";
 $mysql_password = "greenbat";
 $mysql_database = "cartoonbankru";
-
 $limit = 4;
 $limit_plus = 3;
 $limit_minus = 4;
 $limit_black = 1;
-
 $bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
 mysql_select_db($mysql_database, $bd) or die("Could not select database");
-
 ?>
