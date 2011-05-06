bae4d137ba4647643f096edc8e51bde6f0ecd288
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-05-06 17:27:34 +0000

    invoice.html

--- trunk/wp-content/plugins/wp-shopping-cart/display-clients.php
+++ trunk/wp-content/plugins/wp-shopping-cart/display-clients.php
@@ -183,7 +183,7 @@ if($product_list != null)
 		echo "            </td>";
 
 		
-		echo "            <td class='t' style='width:250px;'>";
+		echo "            <td class='t' style='width:300px;font-size:0.8em;'>";
 		echo $product['bank_attributes'];
 		echo "            </td>";
 
@@ -228,7 +228,7 @@ if (isset($_GET['m']) && is_numeric($_GET['m']))
 
 
 	// all clients ids
-	$sql = "select id, user_id, name from al_customers order by contract";
+	$sql = "select id, user_id, name, bank_attributes, contract, contract_date from al_customers order by contract";
 	$response = $wpdb->get_results($sql,ARRAY_A) ;
 	if($response != null)
 	  {
@@ -263,15 +263,40 @@ if (isset($_GET['m']) && is_numeric($_GET['m']))
 									///pokazh($sql);
 				if($product_list != null)
 					{
-					echo ("<div class='t' style='background-color:#CCD2FF;padding-left:4px;margin-top:8px;'>".$product['name']."</div>");
+						$n = 1;
+						$count = count($product_list);
+						$total = 0;
+						$the_list = '';
+						$contract_period = $_month.".".date("Y");
+
+						echo ("<div class='t' style='background-color:#CCD2FF;padding-left:4px;margin-top:8px;'>".$product['name']."</div>");
+					
 					  foreach($product_list as $sales)
 						{
-							echo "<div class='t'>".date("d.m.y H:m:s",$sales['date'])." <b>".$sales['artist']."</b> «".stripslashes($sales['title'])."» цена:".round($sales['price'],0)." скидка:".round($sales['discount'],0)." итого:<b>".round($sales['price'],0)*((100-round($sales['discount'],0))/100)."</b></div>";
-						}//foreach($product_list as $product)
+							$discount_price = round($sales['price'],0)*((100-round($sales['discount'],0))/100);
+							
+							echo "<div class='t'>".date("d.m.y H:m:s",$sales['date'])." <b>".$sales['artist']."</b> «".stripslashes($sales['title'])."» цена:".round($sales['price'],0)." скидка:".round($sales['discount'],0)." итого:<b>".$discount_price."</b></div>";
+							
+							$total = $total + $discount_price;
+							
+							$the_list .= "<tr>
+												<td style='padding:2px;text-align:center;'>".$n."</td>
+												<td style='padding:2px;text-align:center;'>".$sales['purchaseid']."</td>
+												<td style='font-style:bold;font-size:0.7em;padding:2px;'>«".stripslashes($sales['title'])."» (#".$sales['id'].") ".$sales['artist']."</td>
+												<td style='padding:2px;text-align:center;'>1</td>
+												<td style='padding:2px;text-align:center;'>шт.</td>
+												<td style='padding:2px;text-align:center;'>".$discount_price."</td>
+												<td style='padding:2px;text-align:center;'>".$discount_price."</td>
+											 </tr>";
+
+							$n++;
+						}//foreach($product_list as $sales)
 					
+
 						// print invoice:
-						echo "<div id='invoice'>";
-						echo fill_invoice();
+						echo "<div id='invoice' style='margin:20px;padding:2px;background-color:#F0F1B8;width: 210mm; border: 1px #efefef solid; font-size: 11pt;'>";
+						echo fill_invoice('', '', $product['bank_attributes'].". ".$product['name'], $the_list, $total, $count, $contract_period, $product['contract'],date_format(date_create($product['contract_date']),'d-m-Y'));
+						//fill_invoice($invoice_number='',$invoice_date='',$client_details='',$product_list='',$total='',$count='',$invoice_period='',$contract_number='',$contract_date='')
 						echo "</div>";
 
 					}//if($product_list != null)
@@ -283,30 +308,31 @@ if (isset($_GET['m']) && is_numeric($_GET['m']))
 }//if (isset($_GET['m']) && is_numeric($_GET['m']))
 
 
-function fill_invoice($invoice_number='',$invoice_date='',$client_details='',$product_list='',$total='')
+function fill_invoice($invoice_number='',$invoice_date='',$client_details='',$product_list='',$total='',$count='',$invoice_period='',$contract_number='',$contract_date='')
 {
-
 	if ($invoice_number==''){$invoice_number='____';}
-	if ($invoice_date==''){$invoice_date = getdate();}
-	if ($client_details==''({$client_details = 'данные покупателя неизвестны';}
+	$today = getdate();
+	if ($invoice_date==''){$invoice_date = $today[mday].".".$today[mon].".".$today[year];}
+	if ($client_details==''){$client_details = 'данные покупателя неизвестны';}
 	if ($product_list=='')
 		{
 		$product_list = '<tr>
-			<td style="width:13mm;"></td>
-			<td style="width:20mm;"></td>
-			<td></td>
-			<td style="width:20mm;"></td>
-			<td style="width:17mm;"></td>
-			<td style="width:27mm;">Цена
-			<td style="width:27mm;"></td>
+			<td style="width:13mm;">&nbsp;</td>
+			<td style="width:20mm;">&nbsp;</td>
+			<td>&nbsp;</td>
+			<td style="width:20mm;">&nbsp;</td>
+			<td style="width:17mm;">&nbsp;</td>
+			<td style="width:27mm;">&nbsp;</td>
+			<td style="width:27mm;">&nbsp;</td>
 			</tr>';
 		}
 	if ($total==''){$total=0;}
+	if ($count==''){$count=0;}
 
-		$filename = "invoice.html";
+		$filename = "/home/www/cb3/wp-content/plugins/wp-shopping-cart/invoice.html";
 		$content=loadFile($filename); 
 
-		$total_rub_text = $total;
+		$total_rub_text = "<b>".num2str($total)."</b>";
 
 	// replace placeholders
 		$content = str_replace ('{invoice_number}',$invoice_number,$content);
@@ -314,7 +340,11 @@ function fill_invoice($invoice_number='',$invoice_date='',$client_details='',$pr
 		$content = str_replace ('{client_details}',$client_details,$content);
 		$content = str_replace ('{product_list}',$product_list,$content);
 		$content = str_replace ('{total}',$total,$content);
+		$content = str_replace ('{count}',$count,$content);
 		$content = str_replace ('{total_rub_text}',$total_rub_text,$content);
+		$content = str_replace ('{invoice_period}',$invoice_period,$content);
+		$content = str_replace ('{contract_number}',$contract_number,$content);
+		$content = str_replace ('{contract_date}',$contract_date,$content);
 
 		// output content
 	return $content;
@@ -338,5 +368,85 @@ function loadFile($sFilename, $sCharset = 'UTF-8')
     if ($sEncoding = mb_detect_encoding($sData, 'auto', true) != $sCharset)
         $sData = mb_convert_encoding($sData, $sCharset, $sEncoding);
     return $sData;
+}
+
+function num2str($inn, $stripkop=false) {
+    $nol = 'ноль';
+    $str[100]= array('','сто','двести','триста','четыреста','пятьсот','шестьсот', 'семьсот', 'восемьсот','девятьсот');
+    $str[11] = array('','десять','одиннадцать','двенадцать','тринадцать', 'четырнадцать','пятнадцать','шестнадцать','семнадцать', 'восемнадцать','девятнадцать','двадцать');
+    $str[10] = array('','десять','двадцать','тридцать','сорок','пятьдесят', 'шестьдесят','семьдесят','восемьдесят','девяносто');
+    $sex = array(
+        array('','один','два','три','четыре','пять','шесть','семь', 'восемь','девять'),// m
+        array('','одна','две','три','четыре','пять','шесть','семь', 'восемь','девять') // f
+    );
+    $forms = array(
+        array('копейка', 'копейки', 'копеек', 1), // 10^-2
+        array('рубль', 'рубля', 'рублей',  0), // 10^ 0
+        array('тысяча', 'тысячи', 'тысяч', 1), // 10^ 3
+        array('миллион', 'миллиона', 'миллионов',  0), // 10^ 6
+        array('миллиард', 'миллиарда', 'миллиардов',  0), // 10^ 9
+        array('триллион', 'триллиона', 'триллионов',  0), // 10^12
+    );
+    $out = $tmp = array();
+    // Поехали!
+    $tmp = explode('.', str_replace(',','.', $inn));
+    $rub = number_format($tmp[ 0], 0,'','-');
+    if ($rub== 0) $out[] = $nol;
+    // нормализация копеек
+    $kop = isset($tmp[1]) ? substr(str_pad($tmp[1], 2, '0', STR_PAD_RIGHT), 0,2) : '00';
+    $segments = explode('-', $rub);
+    $offset = sizeof($segments);
+    if ((int)$rub== 0) { // если 0 рублей
+        $o[] = $nol;
+        $o[] = morph( 0, $forms[1][ 0],$forms[1][1],$forms[1][2]);
+    }
+    else {
+        foreach ($segments as $k=>$lev) {
+            $sexi= (int) $forms[$offset][3]; // определяем род
+            $ri = (int) $lev; // текущий сегмент
+            if ($ri== 0 && $offset>1) {// если сегмент==0 & не последний уровень(там Units)
+                $offset--;
+                continue;
+            }
+            // нормализация
+            $ri = str_pad($ri, 3, '0', STR_PAD_LEFT);
+            // получаем циферки для анализа
+            $r1 = (int)substr($ri, 0,1); //первая цифра
+            $r2 = (int)substr($ri,1,1); //вторая
+            $r3 = (int)substr($ri,2,1); //третья
+            $r22= (int)$r2.$r3; //вторая и третья
+            // разгребаем порядки
+            if ($ri>99) $o[] = $str[100][$r1]; // Сотни
+            if ($r22>20) {// >20
+                $o[] = $str[10][$r2];
+                $o[] = $sex[ $sexi ][$r3];
+            }
+            else { // <=20
+                if ($r22>9) $o[] = $str[11][$r22-9]; // 10-20
+                elseif($r22> 0) $o[] = $sex[ $sexi ][$r3]; // 1-9
+            }
+            // Рубли
+            $o[] = morph($ri, $forms[$offset][ 0],$forms[$offset][1],$forms[$offset][2]);
+            $offset--;
+        }
+    }
+    // Копейки
+    if (!$stripkop) {
+        $o[] = $kop;
+        $o[] = morph($kop,$forms[ 0][ 0],$forms[ 0][1],$forms[ 0][2]);
+    }
+    return preg_replace("/\s{2,}/",' ',implode(' ',$o));
+}
+ 
+/**
+ * Склоняем словоформу
+ */
+function morph($n, $f1, $f2, $f5) {
+    $n = abs($n) % 100;
+    $n1= $n % 10;
+    if ($n>10 && $n<20) return $f5;
+    if ($n1>1 && $n1<5) return $f2;
+    if ($n1==1) return $f1;
+    return $f5;
 }
 ?>
