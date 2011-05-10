512f342404a2761eade32fa25e1353dacd7a99bb
igor.aleshin@gmail.com <igor.aleshin@gmail.com@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-05-10 19:20:15 +0000

    tcpdf

---
+++ trunk/ales/tcpdf/examples/ales.php
@@ -0,0 +1,117 @@
+<?php
+//============================================================+
+// File name   : example_008.php
+// Begin       : 2008-03-04
+// Last Update : 2010-08-08
+//
+// Description : Example 008 for TCPDF class
+//               Include external UTF-8 text file
+//
+// Author: Nicola Asuni
+//
+// (c) Copyright:
+//               Nicola Asuni
+//               Tecnick.com s.r.l.
+//               Via Della Pace, 11
+//               09044 Quartucciu (CA)
+//               ITALY
+//               www.tecnick.com
+//               info@tecnick.com
+//============================================================+
+
+/**
+ * Creates an example PDF TEST document using TCPDF
+ * @package com.tecnick.tcpdf
+ * @abstract TCPDF - Example: Include external UTF-8 text file
+ * @author Nicola Asuni
+ * @since 2008-03-04
+ */
+if (isset($_POST['html']))
+{
+	$content = $_POST['html'];
+	$content = stripslashes(htmlspecialchars_decode($content));
+}
+else
+{
+	echo ("please send me the code");
+	exit;
+}
+
+
+
+require_once('../config/lang/eng.php');
+require_once('../tcpdf.php');
+
+// create new PDF document
+$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
+$pdf->SetFont('freeserif', '', 10);
+$pdf->SetHeaderFont('freeserif', '', 10);
+
+
+
+// set document information
+$pdf->SetCreator(PDF_CREATOR);
+$pdf->SetAuthor('Nicola Asuni');
+$pdf->SetTitle('Счёт');
+$pdf->SetSubject('TCPDF Tutorial');
+$pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+
+
+// set default header data
+$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' ', PDF_HEADER_STRING);
+
+// set header and footer fonts
+$pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+$pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+
+$pdf->setPrintFooter(false);
+
+// set default monospaced font
+$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+//set margins
+$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+
+//set auto page breaks
+$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+
+//set image scale factor
+$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+//set some language-dependent strings
+$pdf->setLanguageArray($l);
+
+// ---------------------------------------------------------
+
+// set font
+$pdf->SetFont('freeserif', '', 10);
+
+// add a page
+$pdf->AddPage();
+
+// get esternal file content
+//$utf8text = file_get_contents('../cache/utf8test.txt', false);
+
+// set color for text
+$pdf->SetTextColor(0, 63, 127); //blue
+
+$html = $content;
+
+// output the HTML content
+$pdf->writeHTML($html, true, false, true, false, '');
+
+// write the text
+//$pdf->Write(5, $html, '', 0, '', false, 0, false, false, 0);
+
+
+// ---------------------------------------------------------
+
+//Close and output PDF document
+$pdf->Output('invoce.pdf', 'D');
+
+//============================================================+
+// END OF FILE                                                
+//============================================================+
