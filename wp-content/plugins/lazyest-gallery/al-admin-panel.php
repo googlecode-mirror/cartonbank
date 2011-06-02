f93bfd9c0da2dd6cfeef4423ada6c4dcc7b41a63
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-06-02 15:51:21 +0000

    bankir help page

--- wp-content/plugins/lazyest-gallery/al-admin-panel.php
+++ wp-content/plugins/lazyest-gallery/al-admin-panel.php
@@ -1,8 +1,4 @@
-<br />
-На это странице ссылки на описание правил работы с картунбанком, отправки и редактирования изображений.
-<p>
-<a href="http://cartoonbank.ru/?page_id=229" target=_blank>Отправка изображения Автором через редакторскую форму.</a> 
-</p>
-<br>
-<h3>Как заменить картинку?</h3>
-<img src="http://cartoonbank.ru/img/help/replace_image.png">
+<h2>Подсказки и ответы на часто задаваемые вопросы</h2>
+1. <a href="http://cartoonbank.ru/?page_id=229" target=_blank>Отправка изображения Автором через редакторскую форму. Ссылка на инструкцию</a><br>
+2. <a href="http://cartoonbank.ru/img/help/replace_image.png" target="_blank">Как заменить картинку на новую?</a><br>
+3. <a href="http://cartoonbank.ru/img/help/May-2011-report.doc">Отчёт Картунбанка за май 2011</a><br>
