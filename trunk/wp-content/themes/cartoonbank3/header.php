31dc9bf30efeb1fd131b6c443f1cd9ef74c3989c
igor.aleshin@gmail.com <igor.aleshin@gmail.com@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-06-23 07:21:06 +0000

    new yandex ad block

--- trunk/wp-content/themes/cartoonbank3/header.php
+++ trunk/wp-content/themes/cartoonbank3/header.php
@@ -45,20 +45,31 @@ if ($_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'] == "cartoonbank.ru/?page_id=29
 <center><div id="y" style="float:center;width:960px;background-color:white;">
 <!-- Яндекс.Директ -->
 <script type="text/javascript">
-yandex_partner_id = 67799;
-yandex_site_bg_color = 'FFFFFF';
-yandex_site_charset = 'utf-8';
-yandex_ad_format = 'direct';
-yandex_font_size = 1;
-yandex_direct_type = 'horizontal';
-yandex_direct_limit = 4;
-yandex_direct_title_color = '666666';
-yandex_direct_url_color = '333333';
-yandex_direct_all_color = '333333';
-yandex_direct_text_color = '999999';
-yandex_direct_hover_color = '666666';
-yandex_direct_favicon = false;
-document.write('<sc'+'ript type="text/javascript" src="http://an.yandex.ru/system/context.js"></sc'+'ript>');
+(function(w, d, n, s, t) {
+    w[n] = w[n] || [];
+    w[n].push(function() {
+        Ya.Direct.insertInto(67799, "y", {
+            site_charset: "utf-8",
+            ad_format: "direct",
+            font_size: 1,
+            type: "horizontal",
+            limit: 4,
+            title_font_size: 3,
+            site_bg_color: "FFFFFF",
+            title_color: "666666",
+            url_color: "333333",
+            all_color: "333333",
+            text_color: "999999",
+            hover_color: "666666"
+        });
+    });
+    t = d.documentElement.firstChild;
+    s = d.createElement("script");
+    s.type = "text/javascript";
+    s.src = "http://an.yandex.ru/system/context.js";
+    s.setAttribute("async", "true");
+    t.insertBefore(s, t.firstChild);
+})(window, document, "yandex_context_callbacks");
 </script>
 </div></center>
 <?
