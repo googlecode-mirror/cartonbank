b420e572edbff67a2adb53a56ca9223441e31949
igor.aleshin <igor.aleshin@gmail.com>
2011-06-26 08:04:06 +0000

    vychety

--- wp-content/plugins/lazyest-gallery/al-admin-panel.php
+++ wp-content/plugins/lazyest-gallery/al-admin-panel.php
@@ -2,3 +2,4 @@
 1. <a href="http://cartoonbank.ru/?page_id=229" target=_blank>Отправка изображения Автором через редакторскую форму. Ссылка на инструкцию</a><br>
 2. <a href="http://cartoonbank.ru/img/help/replace_image.png" target="_blank">Как заменить картинку на новую?</a><br>
 3. <a href="http://cartoonbank.ru/img/help/May-2011-report.doc">Отчёт Картунбанка за май 2011</a><br>
+4. <a href="http://cartoonbank.ru/?page_id=1775">Заявление на налоговый вычет</a><br>
