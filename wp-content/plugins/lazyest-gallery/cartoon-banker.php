8d160a778ce2a53e4c73dbcce228e743b5cf7884
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-06-27 08:07:21 +0000

    forum menu item added

--- wp-content/plugins/lazyest-gallery/cartoon-banker.php
+++ wp-content/plugins/lazyest-gallery/cartoon-banker.php
@@ -27,6 +27,18 @@ if (isset($current_user->wp_capabilities['editor']) && ($current_user->wp_capabi
 	add_submenu_page('lazyest-gallery/al-admin-panel.php','Тема дня', 'Тема дня', 'read', 'wp-shopping-cart/themeoftheday.php');
 
 
+
+if (isset($current_user->wp_capabilities['administrator']) && ($current_user->wp_capabilities['administrator']==1))
+	add_submenu_page('lazyest-gallery/al-admin-panel.php','Микрофорум', 'Микрофорум', 'read', 'forum_redirect.php');
+
+if (isset($current_user->wp_capabilities['editor']) && ($current_user->wp_capabilities['editor']==1))
+	add_submenu_page('lazyest-gallery/al-admin-panel.php','Микрофорум', 'Микрофорум', 'read', 'forum_redirect.php');
+
+if (isset($current_user->wp_capabilities['author']) && ($current_user->wp_capabilities['author']==1))
+	add_submenu_page('lazyest-gallery/al-admin-panel.php','Микрофорум', 'Микрофорум', 'read', 'forum_redirect.php');
+
+
+
 if (isset($current_user->wp_capabilities['administrator']) && ($current_user->wp_capabilities['administrator']==1))
 	add_submenu_page('lazyest-gallery/al-admin-panel.php','Продажи работ', 'Продажи работ', 'read', 'wp-shopping-cart/allsales.php');
 
