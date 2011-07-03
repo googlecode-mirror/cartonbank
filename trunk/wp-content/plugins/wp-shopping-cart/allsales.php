bf813431da96dd92893ec78b37d8235042f53ce2
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-07-03 19:28:17 +0000

    zarabotano

--- trunk/wp-content/plugins/wp-shopping-cart/allsales.php
+++ trunk/wp-content/plugins/wp-shopping-cart/allsales.php
@@ -52,7 +52,7 @@ else
 	$end_timestamp = mktime(0, 0, 0, ($month+1), 0, $year);
 }
 
-$sql = "SELECT COUNT( * ) as count, temp.name FROM ( SELECT b.id, b.name FROM  `wp_purchase_logs` AS l,  `wp_purchase_statuses` AS s,  `wp_cart_contents` AS c,  `wp_product_list` AS p,  `wp_download_status` AS st,  `wp_product_brands` AS b, `wp_users` AS u WHERE l.`processed` = s.`id`  AND l.id = c.purchaseid AND p.id = c.prodid AND st.purchid = c.purchaseid AND p.brand = b.id AND u.id = l.user_id AND l.user_id !=  '106' AND st.downloads !=  '5' AND date BETWEEN '$start_timestamp' AND '$end_timestamp' GROUP BY c.license ORDER BY b.name ) AS temp GROUP BY temp.id order by temp.name  
+$sql = "SELECT COUNT( * ) as count, temp.id, temp.name, temp.user_id FROM ( SELECT b.id, b.name, b.user_id FROM  `wp_purchase_logs` AS l,  `wp_purchase_statuses` AS s,  `wp_cart_contents` AS c,  `wp_product_list` AS p,  `wp_download_status` AS st,  `wp_product_brands` AS b, `wp_users` AS u WHERE l.`processed` = s.`id`  AND l.id = c.purchaseid AND p.id = c.prodid AND st.purchid = c.purchaseid AND p.brand = b.id AND u.id = l.user_id AND l.user_id !=  '106' AND st.downloads !=  '5' AND date BETWEEN '$start_timestamp' AND '$end_timestamp' GROUP BY c.license ORDER BY b.name ) AS temp GROUP BY temp.id order by temp.name  
 ";
 
 $result = $wpdb->get_results($sql,ARRAY_A);
@@ -63,10 +63,31 @@ echo "<div><table style='width:250px;background-color:#E8E8E8;'>";
 foreach ($result as $row)
 {
 	echo "<tr class='alternate'>";
-	echo "<td class='username' style='padding:4px;width:200px;'>".$row['name']."</td><td class='posts' style='padding:4px;wisth:200px;text-align:right;'>".$row['count']."</td>";
+	//todo
+	if ($current_user->wp_user_level ==10)
+	{	
+		echo "<td class='username' style='padding:4px;width:200px;'><a href='http://cartoonbank.ru/wp-admin/admin.php?page=wp-shopping-cart/display_artist_income.php&brand=".$row['id']."'>".$row['name']."</a></td><td class='posts' style='padding:4px;wisth:200px;text-align:right;'>".$row['count']."</td>";
+	}
+	else
+		{	
+		if ($current_user->id == $row['user_id']) 
+			{
+				echo "<td class='username' style='padding:4px;width:200px;'><a href='http://cartoonbank.ru/wp-admin/admin.php?page=wp-shopping-cart/display_artist_income.php&brand=".$row['id']."'>".$row['name']."</a></td><td class='posts' style='padding:4px;wisth:200px;text-align:right;'>".$row['count'];
+			}
+			else
+			{
+				echo "<td class='username' style='padding:4px;width:200px;'>".$row['name']."</td><td class='posts' style='padding:4px;wisth:200px;text-align:right;'>".$row['count'];
+			}
+
+		echo "</td>";
+	}
+	//echo "<td class='username' style='padding:4px;width:200px;'><a href='".get_option('site_url')."/cb/wp-admin/admin.php?page=wp-shopping-cart/display_artist_income.php&brand=".$row['id']."'>".$row['name']."</a></td><td class='posts' style='padding:4px;wisth:200px;text-align:right;'>".$row['count']."</td>";
+
 	echo "</tr>";
 }
 echo "</table></div>";
+
+//pokazh($current_user->id,"id");
 
 echo "<br></div>";
 
