56569eff8ce03cda337380ce331057462e039b3b
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-07-03 19:31:13 +0000

    zarabotano

--- wp-content/plugins/lazyest-gallery/cartoon-banker.php
+++ wp-content/plugins/lazyest-gallery/cartoon-banker.php
@@ -49,6 +49,31 @@ if (isset($current_user->wp_capabilities['author']) && ($current_user->wp_capabi
 	add_submenu_page('lazyest-gallery/al-admin-panel.php','Продажи работ', 'Продажи работ', 'read', 'wp-shopping-cart/allsales.php');
 
 
+// brand of the logged in user
+/*
+		$sql = "SELECT id FROM `wp_product_brands` WHERE user_id = ".$current_user->id;
+		$userbrand = $wpdb->get_results($sql,ARRAY_A);
+		//pokazh($sql);
+		if($userbrand != null)
+		{
+			$userbrandid = $userbrand[0]['id'];
+			//pokazh($product);
+		}
+		else
+		{
+			$userbrandid = 0;
+		}
+*/
+if (isset($current_user->wp_capabilities['administrator']) && ($current_user->wp_capabilities['administrator']==1))
+	add_submenu_page('lazyest-gallery/al-admin-panel.php','Заработано', 'Заработано', 'read', 'wp-shopping-cart/display_artist_income.php');
+
+if (isset($current_user->wp_capabilities['editor']) && ($current_user->wp_capabilities['editor']==1))
+	add_submenu_page('lazyest-gallery/al-admin-panel.php','Заработано', 'Заработано', 'read', 'wp-shopping-cart/display_artist_income.php');
+
+if (isset($current_user->wp_capabilities['author']) && ($current_user->wp_capabilities['author']==1))
+	add_submenu_page('lazyest-gallery/al-admin-panel.php','Заработано', 'Заработано', 'read', 'wp-shopping-cart/display_artist_income.php');
+
+
 //add_submenu_page('lazyest-gallery/al-admin-panel.php','Прихожая', 'Прихожая', 'read', 'purgatory/purgatory.php');
 add_submenu_page('lazyest-gallery/al-admin-panel.php','Редактор базы изображений', 'Редактор базы', 'read', 'wp-shopping-cart/display-items.php');
 /*
