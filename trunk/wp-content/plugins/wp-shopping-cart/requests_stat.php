1ef853874cb70ffbc35bd2ea487e320cf5624a00
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-08-17 08:01:56 +0000

    requests_stat.php

--- trunk/wp-content/plugins/wp-shopping-cart/requests_stat.php
+++ trunk/wp-content/plugins/wp-shopping-cart/requests_stat.php
@@ -39,36 +39,37 @@ else
 
 // Popular requests
 
-	$sql = "SELECT term, count(id) as cnt FROM `search_terms` group by term order by cnt desc, term LIMIT 100";
+	$sql = "SELECT term, count(id) as cnt FROM `search_terms` group by term order by cnt desc, term LIMIT 150";
 
 	$result = $wpdb->get_results($sql,ARRAY_A);
 	if (!$result) {die('<br />'.$del_sql.'<br />Invalid select query: ' . mysql_error());}
 
-	echo "<div style='vertical-align:top;'><div><h3>Статистика поисковых запросов (100 популярных)</h3></div>";
+	echo "<div style='vertical-align:top;'><div><h3>Статистика поисковых запросов</h3></div>";
 	echo "<a href='#new'>Ниже находится таблица с новыми запросами</a>";
 
-	echo "<div style='background-color:#FFFF99; padding:4px;font-size:0.8em;margin-bottom:6px;'>Суммарная статистика по поисковым словам учитывает <br>1) слова набранные в поисковой форме; </br>2) переходы по тэгам, указанным в описании.</div>";
-	echo "<div><table style='background-color:#E8E8E8;'>";
+	echo "<div style='padding:4px;font-size:0.8em;margin-bottom:6px;'>Суммарная статистика по поисковым словам учитывает <br>1) слова набранные в поисковой форме; 2) переходы по тэгам, указанным в описании.</div>";
 
-	$counter = 1;
+	echo "<div style='vertical-align:top;float:left;'><div><a name='pop'><h3>150 популярных запросов</h3></a></div>";
 
-	foreach ($result as $row)
-	{
-		echo "<tr class='alternate'>";
-		//todo
-			echo "<td style='text-align:right; padding-right:2px;color:silver;'>$counter</td>";		
-			echo "<td class='username' style='padding:4px;'><a href='http://cartoonbank.ru/?page_id=29&cs=".$row['term']."'>".urldecode($row['term'])."</a></td><td class='posts' style='padding:4px;text-align:right;'>".$row['cnt']."</td>";
+		echo "<div style='width:1000px;clear:all;'>";
 
-		echo "</tr>";
+		$counter = 1;
 
-		$counter = $counter + 1;
-	}
-	echo "</table></div>";
+		foreach ($result as $row)
+		{
+			echo "<div style='padding:2px;margin:2px;background-color:#D0FE7A;float:left;'>";
+			//todo
+				echo "<span style='padding-right:2px;color:silver;'>$counter</span>";		
+				echo "<span class='username' style='padding:4px;'><a href='http://cartoonbank.ru/?page_id=29&cs=".$row['term']."'>".urldecode($row['term'])."</a></span><span class='posts' style='padding:4px;text-align:right;'>".$row['cnt']."</span>";
 
-	//pokazh($current_user->id,"id");
+			echo "</div>";
 
-	echo "<br></div>";
+			$counter = $counter + 1;
+		}
+		echo "</div>";
+	echo "</div>";
 
+echo "<div style='clear:all;'></div>";
 
 // New requests
 
@@ -77,8 +78,8 @@ else
 	$result = $wpdb->get_results($sql,ARRAY_A);
 	if (!$result) {die('<br />'.$del_sql.'<br />Invalid select query: ' . mysql_error());}
 
-	echo "<div style='vertical-align:top;'><div><a name='new'><h3>Статистика поисковых запросов (100 последних)</h3></a></div>";
-	echo "<div style='background-color:#FFFF99; padding:4px;font-size:0.8em;margin-bottom:6px;'>Статистика содержит поисковое слово (слова), дату поиска, айпи-адрес, заголовок браузера.</div>";
+	echo "<div style='vertical-align:top;float:left;'><div><a name='new'><h3>100 последних запросов</h3></a></div>";
+	echo "<div style='padding:4px;font-size:0.8em;margin-bottom:6px;'>Статистика содержит поисковое слово (слова), дату поиска, айпи-адрес, заголовок браузера.</div>";
 	echo "<div><table style='background-color:#E8E8E8;'>";
 
 	$counter = 1;
@@ -88,7 +89,7 @@ else
 		echo "<tr class='alternate'>";
 		//todo
 			echo "<td style='text-align:right; padding-right:2px;color:silver;'>$counter</td>";		
-			echo "<td class='username' style='padding:4px;'><a href='http://cartoonbank.ru/?page_id=29&cs=".$row['term']."'>".urldecode($row['term'])."</a></td><td class='posts' style='padding:4px;width:100px;'>".$row['datetime']."</td>";
+			echo "<td class='username' style='padding:4px;'><a href='http://cartoonbank.ru/?page_id=29&cs=".$row['term']."' style='padding:2px;margin:2px;background-color:#D0FE7A;float:left;'>".urldecode($row['term'])."</a></td><td class='posts' style='padding:4px;width:100px;'>".$row['datetime']."</td>";
 			echo "<td class='posts' style='padding:4px;'>".$row['header']."</td>";
 
 		echo "</tr>";
