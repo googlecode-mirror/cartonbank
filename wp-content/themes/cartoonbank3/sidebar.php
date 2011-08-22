9b331484d15959bb9e271d04357009d70f3a635e
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-08-22 10:24:01 +0000

    theme of the day archive added/ link

--- wp-content/themes/cartoonbank3/sidebar.php
+++ wp-content/themes/cartoonbank3/sidebar.php
@@ -281,6 +281,7 @@ echo "</div>";
 <br /><h2>Разное</h2>
 <div id='last_sales'><a href='?page_id=1299'>100 продаж</a></div>
 <div id='last_sales'><a href='?page_id=1284&ord=82&br=0' title='сто лучших карикатур'>100 лучших</a></div>
+<div id='last_sales'><a href='?page_id=1992' title='сто лучших карикатур'>Темы дня</a></div>
 <div id='best_of_month'><a href='?page_id=643' title='рейтинг карикатур'>Рейтинг</a></div>
 <div id='tags'><a href='?page_id=390'  title='популярные карикатуры'>Тэги</a></div>
 <div style="float:right;width:180px;text-align:right;">
