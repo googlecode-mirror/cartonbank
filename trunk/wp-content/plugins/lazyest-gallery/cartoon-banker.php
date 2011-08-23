2d395c527b23caf95ee12c33fa77063b66c03ba8
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-08-23 10:15:04 +0000

    requests statistics

--- trunk/wp-content/plugins/lazyest-gallery/cartoon-banker.php
+++ trunk/wp-content/plugins/lazyest-gallery/cartoon-banker.php
@@ -49,6 +49,16 @@ if (isset($current_user->wp_capabilities['author']) && ($current_user->wp_capabi
 	add_submenu_page('lazyest-gallery/al-admin-panel.php','Продажи работ', 'Продажи работ', 'read', 'wp-shopping-cart/allsales.php');
 
 
+if (isset($current_user->wp_capabilities['administrator']) && ($current_user->wp_capabilities['administrator']==1))
+	add_submenu_page('lazyest-gallery/al-admin-panel.php','Статистика запросов', 'Статистика запросов', 'read', 'wp-shopping-cart/requests_stat.php');
+
+if (isset($current_user->wp_capabilities['editor']) && ($current_user->wp_capabilities['editor']==1))
+	add_submenu_page('lazyest-gallery/al-admin-panel.php','Статистика запросов', 'Статистика запросов', 'read', 'wp-shopping-cart/requests_stat.php');
+
+if (isset($current_user->wp_capabilities['author']) && ($current_user->wp_capabilities['author']==1))
+	add_submenu_page('lazyest-gallery/al-admin-panel.php','Статистика запросов', 'Статистика запросов', 'read', 'wp-shopping-cart/requests_stat.php');
+
+
 // brand of the logged in user
 /*
 		$sql = "SELECT id FROM `wp_product_brands` WHERE user_id = ".$current_user->id;
