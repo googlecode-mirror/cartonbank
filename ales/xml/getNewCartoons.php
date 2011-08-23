9cb34ffa2d9ce9a4e72dd015742f6e16f71c2711
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-08-23 10:10:31 +0000

    xml

---
+++ ales/xml/getNewCartoons.php
@@ -0,0 +1,5 @@
+<?php 
+$xmlString = "<rss><Cartoon><title>Эстрасенс</title><link>http://cartoonbank.ru/?page_id=29&amp;cartoonid=13107</link><description>Эстрасенс &quot;колдует&quot; над фотографией, а &quot;объект&quot;видит его руки!</description><pubDate>Tue, 19 Jul 2011 08:28:37 +0000</pubDate><guid>http://cartoonbank.ru/?page_id=29&amp;cartoonid=13107</guid></Cartoon></rss>";
+echo ($xmlString);
+
+?>
