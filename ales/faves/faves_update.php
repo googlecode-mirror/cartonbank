76bfa4644289bae2ae391bb954d9fce2d825db96
igor.aleshin <igor.aleshin@gmail.com>
2011-09-22 10:23:15 +0000

    faves

--- ales/faves/faves_update.php
+++ ales/faves/faves_update.php
@@ -39,12 +39,12 @@ if ($upd != 1)
 	if ($count>0)
 	{
 		// fave exists
-		$ret = "<img src='http://109.120.143.27/cb/img/thumbup.jpg' border='0' align=top title='удалить из списка любимых' style='cursor: pointer;'>";
+		$ret = "<img src='http://cartoonbank.ru/img/thumbup.jpg' border='0' align=top title='удалить из списка любимых' style='cursor: pointer;'>";
 	}
 	else
 	{
 		// no faves
-		$ret = "<img src='http://109.120.143.27/cb/img/thumbupp.jpg' border='0' align=top title='добавить в любимое' style='cursor: pointer;'>";
+		$ret = "<img src='http://cartoonbank.ru/img/thumbupp.jpg' border='0' align=top title='добавить в любимое' style='cursor: pointer;'>";
 	}
 }
 else
@@ -55,14 +55,14 @@ else
 		// unset favorite
 		$sql = "delete from favorites where user_id=$uid and image_id=$p";
 		$result = mysql_query("$sql");
-		$ret = "<img src='http://109.120.143.27/cb/img/thumbupp.jpg' border='0' align=top title='добавить в любимое' style='cursor: pointer;'>";
+		$ret = "<img src='http://cartoonbank.ru/img/thumbupp.jpg' border='0' align=top title='добавить в любимое' style='cursor: pointer;'>";
 	}
 	else
 	{
 		//set favotite
 		$sql = "insert ignore into favorites (user_id, image_id) values ($uid, $p)";
 		$result = mysql_query("$sql");
-		$ret = "<img src='http://109.120.143.27/cb/img/thumbup.jpg' border='0' align=top title='удалить из списка любимых' style='cursor: pointer;'>";
+		$ret = "<img src='http://cartoonbank.ru/img/thumbup.jpg' border='0' align=top title='удалить из списка любимых' style='cursor: pointer;'>";
 	}
 }
 mysql_close($link);
