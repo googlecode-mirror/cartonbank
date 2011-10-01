b10557dcb648244c21227747ceed965807aa9f2d
igor.aleshin@gmail.com <igor.aleshin@gmail.com@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-10-01 15:23:30 +0000

    allsales stat

--- trunk/wp-content/plugins/wp-shopping-cart/allsales.php
+++ trunk/wp-content/plugins/wp-shopping-cart/allsales.php
@@ -52,9 +52,9 @@ else
 	$end_timestamp = mktime(0, 0, 0, ($month+1), 0, $year);
 }
 
-$sql = "SELECT COUNT( * ) as count, temp.id, temp.name, temp.user_id FROM ( SELECT b.id, b.name, b.user_id FROM  `wp_purchase_logs` AS l,  `wp_purchase_statuses` AS s,  `wp_cart_contents` AS c,  `wp_product_list` AS p,  `wp_download_status` AS st,  `wp_product_brands` AS b, `wp_users` AS u WHERE l.`processed` = s.`id`  AND l.id = c.purchaseid AND p.id = c.prodid AND st.purchid = c.purchaseid AND p.brand = b.id AND u.id = l.user_id AND l.user_id !=  '106' AND st.downloads !=  '5' AND date BETWEEN '$start_timestamp' AND '$end_timestamp' GROUP BY c.license ORDER BY b.name ) AS temp GROUP BY temp.id order by temp.name  
+$sql = "SELECT COUNT( * ) as cntr, temp.id, temp.name, temp.user_id, atotal FROM ( SELECT b.count as atotal, b.id, b.name, b.user_id FROM  `wp_purchase_logs` AS l,  `wp_purchase_statuses` AS s,  `wp_cart_contents` AS c,  `wp_product_list` AS p,  `wp_download_status` AS st,  `wp_product_brands` AS b, `wp_users` AS u WHERE l.`processed` = s.`id`  AND l.id = c.purchaseid AND p.id = c.prodid AND st.purchid = c.purchaseid AND p.brand = b.id AND u.id = l.user_id AND l.user_id !=  '106' AND st.downloads !=  '5' AND date BETWEEN '$start_timestamp' AND '$end_timestamp' GROUP BY c.license ORDER BY b.name ) AS temp GROUP BY temp.id order by temp.name  
 ";
-
+//pokazh($sql);
 $result = $wpdb->get_results($sql,ARRAY_A);
 if (!$result) {die('<br />'.$del_sql.'<br />Invalid select query: ' . mysql_error());}
 
@@ -63,25 +63,26 @@ echo "<div style='vertical-align:top;width:500px;'>На этой страниц�
 ?>
 <script type="text/javascript" src="//ajax.googleapis.com/ajax/static/modules/gviz/1.0/chart.js"> {"dataSourceUrl":"//docs.google.com/spreadsheet/tq?key=0AtPperB2fdv5dDZDdEk2cEtNNkpWYXhfVWVHdFM0NUE&transpose=0&headers=0&merge=COLS&range=C3%3AC14%2CT3%3AT14&gid=0&pub=1","options":{"reverseCategories":false,"pointSize":"7","backgroundColor":"#FFFFFF","colors":["#c27ba0","#DC3912","#FF9900","#109618","#990099","#0099C6","#DD4477","#66AA00","#B82E2E","#316395"],"vAxis":{"format":"#0.##########"},"logScale":false,"hAxis":{"maxAlternation":1,"maxAlternations":1},"hasLabelsColumn":true,"vAxes":[{"min":null,"title":null,"max":null}],"title":"\u041f\u0440\u043e\u0434\u0430\u0436\u0438, \u0448\u0442","legend":"none","reverseAxis":false,"isStacked":false,"width":515,"height":359},"state":{},"chartType":"AreaChart","chartName":"Chart 3"} </script>
 <?
-echo "<div><table style='width:250px;background-color:#E8E8E8;'>";
+echo "<div><table style='width:500px;background-color:#E8E8E8;'>";
+echo "<tr><td style='text-align: center;'>автор</td><td style='text-align: center;'>продано штук</td><td style='text-align: center;'> % от работ автора</td><td style='text-align: center;'>всего работ автора</td></tr>";
 foreach ($result as $row)
 {
 	echo "<tr class='alternate'>";
 	//todo
 	if ($current_user->wp_user_level ==10)
 	{	
-		echo "<td class='username' style='padding:4px;width:200px;'><a href='http://cartoonbank.ru/wp-admin/admin.php?page=wp-shopping-cart/display_artist_income.php&brand=".$row['id']."'>".$row['name']."</a></td><td class='posts' style='padding:4px;wisth:200px;text-align:right;'>".$row['count']."</td>";
+		echo "<td class='username' style='padding:4px;width:200px;'><a href='http://cartoonbank.ru/wp-admin/admin.php?page=wp-shopping-cart/display_artist_income.php&brand=".$row['id']."'>".$row['name']."</a></td><td class='posts' style='padding:4px;text-align:right;'>".$row['cntr']."</td><td style='padding:4px;text-align:right;'>".round($row['cntr']*100/$row['atotal'],2)."%</td><td style='padding:4px;text-align:right;'>".$row['atotal']."</td>";
 	}
 	else
 		{	
 		if ($current_user->id == $row['user_id']) 
 			{
 				//echo "<td class='username' style='padding:4px;width:200px;'><a href='http://cartoonbank.ru/wp-admin/admin.php?page=wp-shopping-cart/display_artist_income.php&brand=".$row['id']."'>".$row['name']."</a></td><td class='posts' style='padding:4px;wisth:200px;text-align:right;'>".$row['count'];
-				echo "<td class='username' style='padding:4px;width:200px;'>".$row['name']."</td><td class='posts' style='padding:4px;wisth:200px;text-align:right;'>".$row['count'];
+				echo "<td class='username' style='padding:4px;width:200px;'>".$row['name']."</td><td class='posts' style='padding:4px;wisth:200px;text-align:right;'>".$row['cntr'];
 			}
 			else
 			{
-				echo "<td class='username' style='padding:4px;width:200px;'>".$row['name']."</td><td class='posts' style='padding:4px;wisth:200px;text-align:right;'>".$row['count'];
+				echo "<td class='username' style='padding:4px;width:200px;'>".$row['name']."</td><td class='posts' style='padding:4px;wisth:200px;text-align:right;'>".$row['cntr'];
 			}
 
 		echo "</td>";
