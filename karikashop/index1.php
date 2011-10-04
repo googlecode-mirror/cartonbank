429ad1e4c91703ab26976e66898449d18e208b69
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-10-04 16:02:08 +0000

    Karikashop initial

---
+++ karikashop/index1.php
@@ -0,0 +1,14 @@
+﻿=
+<?
+echo"<pre>";
+echo $_SERVER['REQUEST_URI'];
+$url = $_SERVER['REQUEST_URI'];
+
+$newurl = str_replace("cartoonist.name", "http://cartoonists.cartoonbank.ru/", $url);
+echo $newurl;
+
+print_r ($_SERVER);
+echo"</pre>";
+
+?>
+.
