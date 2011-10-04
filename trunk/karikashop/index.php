26fa8cac01b3f7f6da08702110fbfee6a278408f
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-10-04 16:02:08 +0000

    Karikashop initial

---
+++ trunk/karikashop/index.php
@@ -0,0 +1,21 @@
+<div style="width:960px;" id="main">
+<h1><a href="http://karikashop.com/"><img src="img/ks-logo.gif" border="0"></a></h1>
+
+<div>
+<script type="text/javascript" src="http://app.ecwid.com/script.js?723101" charset="utf-8"></script>
+<script type="text/javascript"> xCategories("style="); </script>
+</div>
+
+<div>
+<script type="text/javascript" src="http://app.ecwid.com/script.js?723101" charset="utf-8"></script>
+<script type="text/javascript"> xProductBrowser("categoriesPerRow=3","views=grid(3,3) list(10) table(20)","categoryView=grid","searchView=list","style="); </script>
+<noscript>Ваш браузер не поддерживает JavaScript. Пожалуйста, перейдите на <a href="http://app.ecwid.com/jsp/723101/catalog">HTML версию Igor Aleshin's store</a></noscript>
+</div>
+
+<div>
+<script type="text/javascript" src="http://app.ecwid.com/script.js?723101" charset="utf-8"></script>
+<!-- remove layout parameter if you want to position minicart yourself -->
+<script type="text/javascript"> xMinicart("style=","layout=attachToCategories"); </script>
+</div>
+
+</div> <!-- main -->
