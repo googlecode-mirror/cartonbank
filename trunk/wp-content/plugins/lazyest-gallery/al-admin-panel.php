7212bbc389e7b579fc4eb7cf52959aa36df93095
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-10-06 15:48:26 +0000

    report added

--- trunk/wp-content/plugins/lazyest-gallery/al-admin-panel.php
+++ trunk/wp-content/plugins/lazyest-gallery/al-admin-panel.php
@@ -3,3 +3,4 @@
 2. <a href="http://cartoonbank.ru/img/help/replace_image.png" target="_blank">Как заменить картинку на новую?</a><br>
 3. <a href="http://cartoonbank.ru/img/help/May-2011-report.doc">Отчёт Картунбанка за май 2011</a><br>
 4. <a href="http://cartoonbank.ru/?page_id=1775">Заявление на налоговый вычет</a><br>
+5. <a href="http://cartoonbank.ru/img/help/report_september_2011.docx">Отчёт Картунбанка за сентябрь 2011</a><br>
