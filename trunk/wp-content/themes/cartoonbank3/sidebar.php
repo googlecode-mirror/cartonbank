fc1b63a195ffbf881d8af966ab1b26518a37e59a
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-10-14 14:06:53 +0000

    &amp;

--- trunk/wp-content/themes/cartoonbank3/sidebar.php
+++ trunk/wp-content/themes/cartoonbank3/sidebar.php
@@ -25,14 +25,14 @@ if (isset($_GET['brand']) && is_numeric($_GET['brand']))
 		}
 
 ?>
-	<div id="sidebar">
-	<h2 style="margin-top:0;">Поиск</h2>
-	<form method="post" id="searchform" action="?page_id=29">
-	<input id="s" size="26" type="text" value="введите поисковое слово..." name="cs" onfocus="this.value='';" style='width:178x;' /><br />
-	<input type="hidden" id="brand" name="brand" value="<?echo $brandid;?>" />
-	<input type="submit" id="searchsubmit" class='borders' value="Искать" style="margin-top:6px;margin-bottom:4px;" />
-	</form>
-	<a href="?page_id=927">Расширенный поиск <img src='http://cartoonbank.ru/img/link.gif' border='0'></a>
+<div id="sidebar">
+<h2 style="margin-top:0;">Поиск</h2>
+<form method="post" id="searchform" action="?page_id=29">
+<input id="s" size="26" type="text" value="введите поисковое слово..." name="cs" onfocus="this.value='';" style='width:178px;' /><br />
+<input type="hidden" id="brand" name="brand" value="<?echo $brandid;?>" />
+<input type="submit" id="searchsubmit" class='borders' value="Искать" style="margin-top:6px;margin-bottom:4px;" />
+</form>
+<a href="?page_id=927">Расширенный поиск <img src='http://cartoonbank.ru/img/link.gif'></a>
 <?
 // Theme of the day
 	$thedate = date("Y.m.d");
@@ -62,7 +62,7 @@ if ($cartoon_of_the_day_id != 0){
 <br /><br />
 <div style="text-align:center; padding-top:6px;  padding-bottom:16px; width:180px;height:186px;background-color:#668bb7;">
 <div style="color:white; padding-bottom:4px;"><b>ТЕМА ДНЯ</b></div>
-<div  style="text-align:center; margin-left:11px; padding-top:6px; width:158px;height:164px;background-color:white;"><a href="<?echo get_option('siteurl');?>/?page_id=29&category=777"><img src="<?echo get_option('siteurl')?>/wp-content/plugins/wp-shopping-cart/images/<?echo $image_name;?>" title="<?echo $image_title;?>" alt="<?echo $image_title;?>" class="thumb"></a><div id='comment' style='font-size:0.8em;'><?echo stripslashes($image_comment);?></div></div>
+<div  style="text-align:center; margin-left:11px; padding-top:6px; width:158px;height:164px;background-color:white;"><a href="<?echo get_option('siteurl');?>/?page_id=29&amp;category=777"><img src="<?echo get_option('siteurl')?>/wp-content/plugins/wp-shopping-cart/images/<?echo $image_name;?>" title="<?echo $image_title;?>" alt="<?echo $image_title;?>" class="thumb"></a><div id='comment' style='font-size:0.8em;'><?echo stripslashes($image_comment);?></div></div>
 </div>
 
 <?}?>
@@ -126,9 +126,9 @@ $total_cartoons = $total_cartoons - 1;
 		else
 		{
 			if (is_numeric($brandid))
-			$category_in_the_list = "<a href='".get_option('product_list_url').$seperator."&brand=".$brandid."&category=".$option['id']."' title='".stripslashes($option['description'])."'>".stripslashes($option['name'])."";
+			$category_in_the_list = "<a href='".get_option('product_list_url').$seperator."&amp;brand=".$brandid."&amp;category=".$option['id']."' title='".stripslashes($option['description'])."'>".stripslashes($option['name'])."";
 			else
-			$category_in_the_list = "<a href='".get_option('product_list_url').$seperator."&category=".$option['id']."' title='".stripslashes($option['description'])."'>".stripslashes($option['name'])."";
+			$category_in_the_list = "<a href='".get_option('product_list_url').$seperator."&amp;category=".$option['id']."' title='".stripslashes($option['description'])."'>".stripslashes($option['name'])."";
 		}
 		
 		foreach ($category_count as $cat_row)
@@ -158,7 +158,7 @@ $total_cartoons = $total_cartoons - 1;
           {
           foreach($subcategories as $subcategory)
             {
-            $options .= "<li><a class='categorylink' href='".get_option('product_list_url').$seperator."&category=".$subcategory['id']."'>-".stripslashes($subcategory['name']);
+            $options .= "<li><a class='categorylink' href='".get_option('product_list_url').$seperator."&amp;category=".$subcategory['id']."'>-".stripslashes($subcategory['name']);
 			$options .= "</a></li>";
             }
           }
@@ -190,7 +190,7 @@ if (!$author_section) // for not Author section (portfolio)
 
 		$_selected = "";
 		if (!isset($_GET['brand'])) {$_selected = ' selected ';}
-		$authors = "<select name='authors' onchange=\"if(!options[selectedIndex].defaultSelected) location='".get_option('siteurl')."/?page_id=29&brand='+options[selectedIndex].value\" style=\"width:180px;margin-top:2px;\"><option ".$_selected." value=''>&nbsp;все авторы&nbsp;</option>";
+		$authors = "<select name='authors' onchange=\"if(!options[selectedIndex].defaultSelected) location='".get_option('siteurl')."/?page_id=29&amp;brand='+options[selectedIndex].value\" style=\"width:180px;margin-top:2px;\"><option ".$_selected." value=''>&nbsp;все авторы&nbsp;</option>";
 		$_selected = "";
 
 		foreach ($brands as $brand)
@@ -233,7 +233,7 @@ else
 		$_selected = "";
 		if (!isset($_GET['brand'])) {$_selected = ' selected ';}
 
-		$authors = "<select name='authors' onchange=\"if(!options[selectedIndex].defaultSelected) location='".get_option('siteurl')."/?page_id=29&brand='+options[selectedIndex].value\" style=\"width:180px;margin-top:2px;\"><option ".$_selected." value=''>&nbsp;все авторы&nbsp;</option>";
+		$authors = "<select name='authors' onchange=\"if(!options[selectedIndex].defaultSelected) location='".get_option('siteurl')."/?page_id=29&amp;brand='+options[selectedIndex].value\" style=\"width:180px;margin-top:2px;\"><option ".$_selected." value=''>&nbsp;все авторы&nbsp;</option>";
 		$_selected = "";
 
 		$thename = "Автор";
@@ -252,10 +252,8 @@ else
 	<?
 	echo $avatar_url."<br />";
 	echo $authors;
-	//echo "<br /><a href='".get_option('siteurl')."/?page_id=29&brand=".$brandid."&bio=1'>Информация об авторе</a>";
 	echo "<br /><a class='example8' href='#'>Информация об авторе</a>";
-	
-	echo "<br /><a href='".get_option('siteurl')."/?page_id=1284&ord=82&br=".$brandid."'>100 лучших работ</a>";
+	echo "<br /><a href='".get_option('siteurl')."/?page_id=1284&amp;ord=82&amp;br=".$brandid."'>100 лучших работ</a>";
 
 	if (isset($brand_result[0]['shop_url']) && $brand_result[0]['shop_owner']==1)
 	{
@@ -273,15 +271,15 @@ echo "</div>";
       {
 		if ($author_section)
 		{
-		   $options = "<a href='".get_option('product_list_url').$seperator."&brand=".$brandid."&color=all&offset=0'>Все изображения [".$total_cartoons."]</a><br />";
-		   $options .= "<a href='".get_option('product_list_url').$seperator."&brand=".$brandid."&color=color' title='цветные карикатуры'>Цветные [".$color_number."]</a><br />";
-		   $options .= "<a href='".get_option('product_list_url').$seperator."&brand=".$brandid."&color=bw' title='черно-белые карикатуры'>Чёрно-белые [".$bw_number."]</a><br />";
+		   $options = "<a href='".get_option('product_list_url').$seperator."&amp;brand=".$brandid."&amp;color=all&amp;offset=0'>Все изображения [".$total_cartoons."]</a><br />";
+		   $options .= "<a href='".get_option('product_list_url').$seperator."&amp;brand=".$brandid."&amp;color=color' title='цветные карикатуры'>Цветные [".$color_number."]</a><br />";
+		   $options .= "<a href='".get_option('product_list_url').$seperator."&amp;brand=".$brandid."&amp;color=bw' title='черно-белые карикатуры'>Чёрно-белые [".$bw_number."]</a><br />";
 		}
 		else
 		{
-		   $options = "<a href='".get_option('product_list_url').$seperator."&&color=all&offset=0'>Все изображения [".$total_cartoons."]</a><br />";
-		   $options .= "<a href='".get_option('product_list_url').$seperator."&color=color' title='цветные карикатуры'>Цветные [".$color_number."]</a><br />";
-		   $options .= "<a href='".get_option('product_list_url').$seperator."&color=bw' title='черно-белые карикатуры'>Чёрно-белые [".$bw_number."]</a><br />";
+		   $options = "<a href='".get_option('product_list_url').$seperator."&&amp;color=all&amp;offset=0'>Все изображения [".$total_cartoons."]</a><br />";
+		   $options .= "<a href='".get_option('product_list_url').$seperator."&amp;color=color' title='цветные карикатуры'>Цветные [".$color_number."]</a><br />";
+		   $options .= "<a href='".get_option('product_list_url').$seperator."&amp;color=bw' title='черно-белые карикатуры'>Чёрно-белые [".$bw_number."]</a><br />";
 		}
 
 	   echo $options;
@@ -289,13 +287,13 @@ echo "</div>";
 ?>
 
 <br /><br />
-<a href="http://cartoonbank.ru/?page_id=1351" target="_blank"><img src="http://cartoonbank.ru/img/b/exhibition_package.gif" width="180" border="0"></a>
+<a href="http://cartoonbank.ru/?page_id=1351" target="_blank"><img src="http://cartoonbank.ru/img/b/exhibition_package.gif" width="180"></a>
 <br /><br />
-<a href="http://cartoonbank.ru/?page_id=1857" target="_blank"><img src="http://cartoonbank.ru/img/b/postard_project.gif" width="180" border="0"></a>
+<a href="http://cartoonbank.ru/?page_id=1857" target="_blank"><img src="http://cartoonbank.ru/img/b/postard_project.gif" width="180"></a>
 
 <br /><h2>Разное</h2>
 <div id='last_sales'><a href='?page_id=1299' title="свежие продажи">Сто продаж</a></div>
-<div id='100_best'><a href='?page_id=1284&ord=72&br=0' title='результаты голосования'>Топ сто</a></div>
+<div id='100_best'><a href='?page_id=1284&amp;ord=72&amp;br=0' title='результаты голосования'>Топ сто</a></div>
 <div id='themes'><a href='?page_id=1992' title='архив тем дня'>Темы дня</a></div>
 <div id='calend'><a href='?page_id=2254' title='календарь-газета'>Календарь</a></div>
 <div id='rating'><a href='?page_id=643' title='рейтинг карикатур'>Рейтинг</a></div>
@@ -306,8 +304,8 @@ echo "</div>";
 	<br /><h2>Вход</h2>
 		<ul style="float:right;width:160px;text-align:right;">
 		<?php wp_register(); ?>
-		<?php wp_loginout(); ?>
-		<?php wp_meta(); ?>
+		<li><?php wp_loginout(); ?></li>
+		<?php //wp_meta(); ?>
 		</ul>
 </div>
 </div>
