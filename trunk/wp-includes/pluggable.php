bb87463223d2cbf5f2ae2431e66dbf2e1558e292
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-10-20 06:58:55 +0000

    cookie longer 30 days

--- trunk/wp-includes/pluggable.php
+++ trunk/wp-includes/pluggable.php
@@ -661,7 +661,7 @@ if ( !function_exists('wp_set_auth_cookie') ) :
  */
 function wp_set_auth_cookie($user_id, $remember = false, $secure = '') {
 	if ( $remember ) {
-		$expiration = $expire = time() + apply_filters('auth_cookie_expiration', 1209600, $user_id, $remember);
+		$expiration = $expire = time() + 2592000; //30 days// apply_filters('auth_cookie_expiration', 1209600, $user_id, $remember);
 	} else {
 		$expiration = time() + apply_filters('auth_cookie_expiration', 172800, $user_id, $remember);
 		$expire = 0;
