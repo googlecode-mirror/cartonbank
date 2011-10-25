747a25bfbd740a5ca9b141b4318a0e4e1436428b
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2011-10-25 05:54:07 +0000

    new upload

--- trunk/ales/upload/index.php
+++ trunk/ales/upload/index.php
@@ -1,3 +1,35 @@
-new files upload 
-<?
-?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Отправка карикатур</title>
+<meta charset="UTF-8" />
+
+<link rel="stylesheet" href="css/main.css"/>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
+<script charset="utf-8" src="js/jquery.client.js"></script>
+
+
+
+</head>
+
+<body>
+
+<div class="errormsg">*</div>
+		
+		<form action="index.php" class="regForm" method="POST" action="http://cartoonbank.ru/ales/upload/savefiles.php">
+		
+				<input type="submit" value="Отправить на сервер">	
+
+					<input type="file" id="fileUpload0" multiple="true" size="60">
+				
+				<input type="submit" value="Отправить на сервер">	
+				
+		</form>
+		
+
+
+</body>
+</html>
+
+
