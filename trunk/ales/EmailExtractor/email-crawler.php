440bf7e8ea0690f898c6f02edcc322537625f179
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2012-02-16 08:12:37 +0000

    EmailExtractor

--- trunk/ales/EmailExtractor/email-crawler.php
+++ trunk/ales/EmailExtractor/email-crawler.php
@@ -1,9 +1,33 @@
+
+
+<html><title>Crawler</title>
+<body>
+<div class="form" style="width:600px; margin-right: auto;margin-left: auto; border:1 solid silver;padding:10px;">
+<br /><br /><form method=post action="#">
+	<center><input type="text" name="urlname" style="margin-right: auto;margin-left: auto; width:500px;font-size: 1.2em;font-color: blue;">
+	<input type="submit"></center>
+</form>
+</div>
+</body></html>
+
+<?
+if ($_REQUEST['urlname'] && strlen(trim($_REQUEST['urlname']))>0)
+    {
+        $starturl = trim($_REQUEST['urlname']);
+    }
+    else
+    {
+        die('<center>Please enter valid start url</center>');
+    }
+?>
 <?php
-$starturl = "http://www.e-kuzbass.ru/vcard/12566/";
+
+set_time_limit (600);
+//$starturl = "http://vechorka.ru/contacts";
 
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL,$starturl);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 30); //timeout after 30 seconds
+    curl_setopt($ch, CURLOPT_TIMEOUT, 600); //timeout after 30 seconds
     curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
     $result=curl_exec ($ch);
     curl_close ($ch);
@@ -15,7 +39,7 @@ $starturl = "http://www.e-kuzbass.ru/vcard/12566/";
         $regexp = "<a\s[^>]*href=(\"??)([^\" >]*?)\\1[^>]*>(.*)<\/a>";
         preg_match_all("/$regexp/siU", $result, $output, PREG_SET_ORDER );
   
-        
+        // array with all links on the page
         $url_array = array();
         
         $i=1;
@@ -25,15 +49,17 @@ $starturl = "http://www.e-kuzbass.ru/vcard/12566/";
             {
                 $item[2] = $rooturl.$item[2];
             }
-        echo $i.". ".$item[2]."<br>";
+        echo "<font color='#3399CC'>".$i.".</font> ".$item[2]."; ";
         array_push($url_array,$item[2]);
         $i++;
         } 
         //echo print_r($url_array);
         
 
-        foreach( $url_array as $item )
+echo "<br><br>Emails found:<br><br>";
 
+		// vizit all links
+        foreach( $url_array as $item )
         {
                 $ch1 = curl_init();
                 curl_setopt($ch1, CURLOPT_URL,$item);
@@ -44,9 +70,10 @@ $starturl = "http://www.e-kuzbass.ru/vcard/12566/";
             
             $email = get_email_from_text ($result);
             fw($email);
-            print($email.", ");
-        }
 
+            print($email);
+        }
+echo "<br>";
     }
     function get_email_from_text ($text){
         
@@ -60,9 +87,12 @@ $starturl = "http://www.e-kuzbass.ru/vcard/12566/";
           );
 
           if ($res) {
-            foreach(array_unique($matches[0]) as $email) {
-                $emails_string .= $email.', ';
-              echo $email . ", ";
+            foreach(array_unique($matches[0]) as $email) 
+			{
+				if (!empty($email)) {
+				  $emails_string .= $email.', ';
+				  echo $email . ", ";
+				}
             }
           }
           else {
@@ -117,9 +147,12 @@ $starturl = "http://www.e-kuzbass.ru/vcard/12566/";
 
     function fw($text)
     {
-        $fp = fopen('_email_list.txt', 'a');
-        fwrite($fp, $text);
-        fclose($fp);
+		if (strlen(trim($text)>0))
+		{
+			$fp = fopen('_email_list.txt', 'a');
+			fwrite($fp, $text);
+			fclose($fp);
+		}
     }
     
     ?>
