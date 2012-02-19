ce40926afa6b6d1b51348c4665785e5581009f45
igor.aleshin@gmail.com <igor.aleshin@gmail.com@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2012-02-19 17:20:36 +0000

    comments fix

--- trunk/wp-content/plugins/purgatory/add_comment.php
+++ trunk/wp-content/plugins/purgatory/add_comment.php
@@ -1,8 +1,14 @@
 <?php
+if (isset($current_user->wp_capabilities['author']) && $current_user->wp_capabilities['author']==1)
+{
+echo ("<h3>Извините, у вас нет права доступа к этой странице</h3>");
+exit;
+}
+require_once("../../../wp-config.php");
 include("config.php");
+$_SITEURL = get_option('siteurl');
+$Current_ID = $current_user->id;
 
-$link = mysql_connect($mysql_hostname, $mysql_user, $mysql_password);
-mysql_set_charset('utf8',$link);
 
 // get comment
 $comment = '';
@@ -20,14 +26,25 @@ if($comment != '')
 	if (isset($_POST['comment_author']))
 		{$cartoon_id = mysql_escape_String($_POST['comment_author']);}
 
-/*
-	<select id="tablefields" name="dummy" size="13" multiple="multiple" ondblclick="insertValueQuery()">
-	<option value="`comment_ID`" title="">comment_ID</option>
-	<option value="`comment_post_ID`" title="">comment_post_ID</option>
-	<option value="`comment_author`" title="">comment_author</option>
-	<option value="`comment_author_email`" title="">comment_author_email</option>
-	<option value="`comment_author_url`" title="">comment_author_url</option>
-	<option value="`comment_author_IP`" title="">comment_author_IP</option>
-	<option value="`comment_date`" title="">comment_date</option>
-	<option value="`comment_date_gmt`" title="">comment_date_gmt</option>
-	<option value="`comment_content`" t
+
+	// save comment:
+	$sql_insert = "insert into wp_comments (comment_post_ID, comment_content, comment_date, comment_author) values('$cartoon_id', '$comment', '".date("Y-m-d H:i:s")."','$author_id')";
+	$result = mysql_query($sql_insert);
+
+
+	//read comments:
+	$result = mysql_query("select C.comment_id, C.comment_content, C.comment_date, U.display_name as author from wp_comments as C, wp_users as U where U.id = C.comment_author order by C.comment_date DESC LIMIT 50");
+
+$result = mysql_query("select C.comment_id, C.comment_content, C.comment_date, U.display_name as author from wp_comments as C, wp_users as U where U.id = C.comment_author order by C.comment_date DESC LIMIT 50");
+	$comments_output = "";
+	while($r = mysql_fetch_array($result)) {
+		$_date = $r['comment_date'];
+		$_comment = nl2br(stripslashes($r['comment_content']));
+		$_author = $r['author'];
+		$_id = $r['comment_id'];
+		$comments_output .= "<div style='margin-top:4px;'><span class='gr' title='".$_date."'>".$_author.":&nbsp; </span><span class='c_body'>".$_comment."</span> [<a title='стереть комментарий' href='#' onclick='deletecomment(".$_id.");'>x</a>]</div>";
+	}
+
+echo $comments_output;
+}
+?>
--- trunk/wp-content/plugins/purgatory/delete_comment.php
+++ trunk/wp-content/plugins/purgatory/delete_comment.php
@@ -1,6 +1,7 @@
-<?php
-
+<?
+require_once("../../../wp-config.php");
 include("config.php");
+$_SITEURL = get_option('siteurl');
 
 if($_POST['id'] or $_GET['id'])
 {
@@ -8,22 +9,30 @@ if (isset($_POST['id']))
 	$id=$_POST['id'];
 elseif (isset($_GET['id']))
 	$id=$_GET['id'];
-
-
 	$id = mysql_escape_String($id);
 
-
+// delete comment	
 	$sql = "delete from wp_comments where Comment_ID=".$id;
-
 	$ip_sql=mysql_query($sql);
-	header('Location: http://cartoonbank.ru/wp-admin/admin.php?page=purgatory/purgatory.php');
-}
 
-function fw($text)
-{
-$fp = fopen('_kloplog.txt', 'a');
-fwrite($fp, $text);
-fclose($fp);
-}
 
+// read comments
+	//read comments:
+	$result = mysql_query("select C.comment_id, C.comment_content, C.comment_date, U.display_name as author from wp_comments as C, wp_users as U where U.id = C.comment_author order by C.comment_date DESC LIMIT 50");
+
+$result = mysql_query("select C.comment_id, C.comment_content, C.comment_date, U.display_name as author from wp_comments as C, wp_users as U where U.id = C.comment_author order by C.comment_date DESC LIMIT 50");
+	$comments_output = "";
+	while($r = mysql_fetch_array($result)) {
+		$_date = $r['comment_date'];
+		$_comment = nl2br(stripslashes($r['comment_content']));
+		$_author = $r['author'];
+		$_id = $r['comment_id'];
+		$comments_output .= "<div style='margin-top:4px;'><span class='gr' title='".$_date."'>".$_author.":&nbsp; </span><span class='c_body'>".$_comment."</span> [<a title='стереть комментарий' href='#' onclick='deletecomment(".$_id.");'>x</a>]</div>";
+	}
+
+echo $comments_output;
+
+
+	//header('Location: '.$_SITEURL.'/wp-admin/admin.php?page=purgatory/purgatory.php');
+}
 ?>
