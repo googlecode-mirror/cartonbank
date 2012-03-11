ccfa27dad0d16f783805ebf214a9a035d548bd0b
igor.aleshin@gmail.com <igor.aleshin@gmail.com@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2012-03-11 14:22:44 +0000

    baner

--- trunk/wp-content/themes/cartoonbank3/sidebar.php
+++ trunk/wp-content/themes/cartoonbank3/sidebar.php
@@ -293,8 +293,8 @@ echo "</div>";
 <br /><br />
 <a href="http://cartoonbank.ru/?page_id=1857" target="_blank"><img src="http://cartoonbank.ru/img/b/postard_project2.gif" style="border:0;width:180px;"></a>
 <br /><br />
-<a href="http://cartoonbank.ru/?page_id=893"><img src="http://cartoonbank.ru/img/b/on-line.gif" style="width:180x;border:0;"></a>
-<br /><br />
+<!-- <a href="http://cartoonbank.ru/?page_id=893"><img src="http://cartoonbank.ru/img/b/on-line.gif" style="width:180x;border:0;"></a>
+<br /><br /> -->
 <a href="http://cartoonbank.ru/?page_id=2479" target="_blank"><img src="http://cartoonbank.ru/img/b/contests.gif" style="border:0;width:180px;"></a>
 
 
