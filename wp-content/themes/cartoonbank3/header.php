a874e9ff1dbb55b6b68902b6fcf62c1da6555886
igor.aleshin <igor.aleshin@gmail.com>
2012-03-29 19:54:25 +0000

    new search big update

--- wp-content/themes/cartoonbank3/header.php
+++ wp-content/themes/cartoonbank3/header.php
@@ -59,7 +59,8 @@ if (current_user_can('manage_options'))
 <style type="text/css" media="screen">
 <!-- @import url( <?php bloginfo('stylesheet_url'); ?> ); -->
 </style>
-<script src="http://109.120.143.27/cb/wp-includes/js/jquery/jquery.highlight-3.js" type="text/javascript"></script>
+href=".get_option('siteurl')."/wp-admin
+<script src="<?echo get_option('siteurl');?>/wp-includes/js/jquery/jquery.highlight-3.js" type="text/javascript"></script>
 <script type="text/javascript">
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-127981-7']);
