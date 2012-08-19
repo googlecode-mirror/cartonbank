1b13719da1b2417e903f85a17e758db8a1042b81
igor.aleshin <igor.aleshin@eeb2e7d5-5883-5e94-f186-6033d15cb3fa>
2012-08-19 15:22:22 +0000

    calendar added to the first page

--- wp-content/themes/cartoonbank3/page.php
+++ wp-content/themes/cartoonbank3/page.php
@@ -31,6 +31,10 @@ else
 </div>
 
 <div style="clear:both;"></div>
+<div id="navbarbottom" style="border-top:5px solid #658DB5;">
+	<iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;height=400&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=9ats0457qmvp1mv5kecdut2uhs%40group.calendar.google.com&amp;color=%23182C57&amp;ctz=Europe%2FMoscow" style=" border-width:0 " width="960" height="400" frameborder="0" scrolling="no"></iframe>
+</div>
+
 <div id="topfooter" style="padding-top:10px;border-top:1px solid #C0C0C0; background: #FFFFFF url(<? echo (get_option('siteurl').'/img/w.png');?>) top center repeat-y;"><h1>Популярные темы. Выбирайте или <a href="http://cartoonbank.ru/?page_id=927" style="color:#CC3300;border:1px solid silver;padding-left:6px;padding-right:6px;">введите своё ключевое слово</a>...</h1>
 <?
 	$filepath = WP_CONTENT_DIR . '/tags/ales-tag_cloud_small.php' ;
