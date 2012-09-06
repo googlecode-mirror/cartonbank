cdc87f5445d04ccd1b5c41d9bfc329e23e2ee5d8
igor.aleshin <igor.aleshin@gmail.com>
2012-09-06 06:33:33 +0000

    script

--- wp-content/plugins/wp-shopping-cart/allsales.php
+++ wp-content/plugins/wp-shopping-cart/allsales.php
@@ -49,7 +49,7 @@ else
 {
 	//$start_timestamp = mktime(0, 0, 0, $month-12, 1, $year);
 	$start_timestamp = mktime(0, 0, 0, 11, 1, 2010);
-	$end_timestamp = mktime(0, 0, 0, ($month+1), 0, $year); 
+	$end_timestamp = mktime(0, 0, 0, ($month+1), 0, $year);
 }
 
 $sql = "SELECT COUNT( * ) as cntr, temp.id, temp.name, temp.user_id, atotal FROM ( SELECT b.count as atotal, b.id, b.name, b.user_id FROM  `wp_purchase_logs` AS l,  `wp_purchase_statuses` AS s,  `wp_cart_contents` AS c,  `wp_product_list` AS p,  `wp_download_status` AS st,  `wp_product_brands` AS b, `wp_users` AS u WHERE l.`processed` = s.`id`  AND l.id = c.purchaseid AND p.id = c.prodid AND st.purchid = c.purchaseid AND p.brand = b.id AND u.id = l.user_id AND l.user_id !=  '106' AND st.downloads !=  '5' AND date BETWEEN '$start_timestamp' AND '$end_timestamp' GROUP BY c.license ORDER BY b.name ) AS temp GROUP BY temp.id order by temp.name  
