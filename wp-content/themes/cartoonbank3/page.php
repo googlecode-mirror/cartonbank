eee7c25cf744a0b8bbab76966a69daee1d9e1d92
igor.aleshin <igor.aleshin@gmail.com>
2013-01-09 13:21:59 +0000

    page.php seo

--- wp-content/themes/cartoonbank3/page.php
+++ wp-content/themes/cartoonbank3/page.php
@@ -35,14 +35,14 @@ else
 	<iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;height=400&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=9ats0457qmvp1mv5kecdut2uhs%40group.calendar.google.com&amp;color=%23182C57&amp;ctz=Europe%2FMoscow" style=" border-width:0 " width="960" height="400" frameborder="0" scrolling="no"></iframe>
 </div>
 
-<div id="topfooter" style="padding-top:10px;border-top:1px solid #C0C0C0; background: #FFFFFF url(<? echo (get_option('siteurl').'/img/w.png');?>) top center repeat-y;"><h1>Популярные темы. Выбирайте или <a href="http://cartoonbank.ru/?page_id=927" style="color:#CC3300;border:1px solid silver;padding-left:6px;padding-right:6px;">введите своё ключевое слово</a>...</h1>
+<div id="topfooter" style="padding-top:10px;border-top:1px solid #C0C0C0; background: #FFFFFF url(<? echo (get_option('siteurl').'/img/w.png');?>) top center repeat-y;"><h4>Популярные темы. Выбирайте или <a href="http://cartoonbank.ru/?page_id=927" style="color:#CC3300;border:1px solid silver;padding-left:6px;padding-right:6px;">введите своё ключевое слово</a>...</h4>
 <?
 	$filepath = WP_CONTENT_DIR . '/tags/ales-tag_cloud_small.php' ;
 	if ( file_exists( $filepath ) )
 	require_once( WP_CONTENT_DIR . '/tags/ales-tag_cloud_small.php' );
 ?>
 </div>
-<div style="text-align:right;background-color:white;border-bottom:1px solid #C0C0C0;"><a href="http://cartoonbank.ru/?page_id=390"><h1>...здесь ещё больше ключевых слов</h1></a></div>
+<div style="text-align:right;background-color:white;border-bottom:1px solid #C0C0C0;"><a href="http://cartoonbank.ru/?page_id=390"><h4>...здесь ещё больше ключевых слов</h4></a></div>
 
 </div>
 <!-- The main column ends  -->
