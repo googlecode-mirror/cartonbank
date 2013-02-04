e2e3948c324c3b10707517a0a20e8006aa112850
igor.aleshin <igor.aleshin@gmail.com>
2013-02-04 14:43:01 +0000

    extra >

--- top-cache.php
+++ top-cache.php
@@ -1,8 +1,8 @@
 <?php
 $file = $cache_file_to_include;;
 if ($file==''){$file='home';}
-$cachefile = '/home/www/cb3/mycache/cached-'.$file.'.html';
-//$cachefile = '/home/www/cb/mycache/cached-'.$file.'.html';
+$thisdir = dirname(__FILE__);
+$cachefile = $thisdir.'/mycache/cached-'.$file.'.html';
 $cachetime = 18000;
 
 // Serve from the cache if it is younger than $cachetime
@@ -12,4 +12,4 @@ if (file_exists($cachefile) && time() - $cachetime < filemtime($cachefile)) {
 }
 $obstarted = TRUE;
 ob_start(); // Start the output buffer
-?>>
+?>
