e09af0f53ed0fd9eff7191b14d3b45cbd59284a4
igor.aleshin <igor.aleshin@gmail.com>
2013-08-31 18:20:52 +0000

    Melnik back

--- wp-content/plugins/wp-shopping-cart/products_page.php
+++ wp-content/plugins/wp-shopping-cart/products_page.php
@@ -14,7 +14,7 @@ if (isset($_REQUEST['new'])&&$_REQUEST['new']==1){$orderBy=' ORDER BY id DESC ';
 if (isset($_REQUEST['new'])&&$_REQUEST['new']==2){$latest=' AND  `wp_product_list`.id > ( SELECT MAX( id ) FROM  `wp_product_list` ) - 10000 ';}else{$latest='';}
 if (isset($_REQUEST['color'])&&$_REQUEST['color']=='color'){$colorfilter=" AND color=1 ";}elseif(isset($_REQUEST['color'])&&$_REQUEST['color']=='bw'){$colorfilter=" AND color = '0'";}else{$colorfilter="";}
 if (isset($_REQUEST['category'])&&is_numeric($_REQUEST['category'])){$categoryid_filter = " AND `wp_product_list`.`category` = '".mysql_escape_string($_REQUEST['category'])."' "; $category=$_REQUEST['category'];}else{$categoryid_filter = '';$category='5';}
-if (isset($_REQUEST['brand'])&&is_numeric($_REQUEST['brand'])){$brandid_filter = " AND `wp_product_list`.`brand` = '".mysql_escape_string($_REQUEST['brand'])."' "; $brandid=mysql_escape_string($_REQUEST['brand']);}else{$brandid_filter=' AND `wp_product_list`.`brand` != 7 ';$brandid=0;}
+if (isset($_REQUEST['brand'])&&is_numeric($_REQUEST['brand'])){$brandid_filter = " AND `wp_product_list`.`brand` = '".mysql_escape_string($_REQUEST['brand'])."' "; $brandid=mysql_escape_string($_REQUEST['brand']);}else{$brandid_filter=' ';$brandid=0;}
 if (isset($_REQUEST['cartoonid'])&&is_numeric($_REQUEST['cartoonid'])){$cartoonid =$_REQUEST['cartoonid'];$cartoonid_filter=" AND `wp_product_list`.`id` = ".$cartoonid." ";}else{$cartoonid=2123;$cartoonid_filter="";}
 
 
@@ -190,7 +190,9 @@ function save_search_terms($terms)
         jQuery(".example8").colorbox({width:"50%", inline:true, href:"#hidden_bio"});
         jQuery(".cb_emailform").colorbox({width:"50%", inline:true, href:"#emailform1"});})
 </script>
-    
+
+
+
 <div style='display:none'>
     <div id='hidden_bio' style='text-align:left; padding:10px; background:#fff;'>
         <? echo ($bio); ?>
